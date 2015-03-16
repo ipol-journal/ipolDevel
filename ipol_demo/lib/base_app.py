77e17db1a9be570a39b5caa7cf05ec4b975da2ee
Miguel Colom <miguel@mcolom.info>
2015-03-16 22:22:40 +0100

    [lib/base_app.py] implemented rectangle and select_subimage to prevent being implemented in app subclasses

--- ipol_demo/lib/base_app.py
+++ ipol_demo/lib/base_app.py
@@ -564,6 +564,117 @@ class base_app(empty_app):
                                     for i in range(self.input_nb)],
                              output=['output.png'])
 
+    def select_subimage(self, x0, y0, x1, y1):
+        """
+        cut subimage from original image
+        """
+        # draw selected rectangle on the image
+        imgS = image(self.work_dir + 'input_0.png')
+        imgS.draw_line([(x0, y0), (x1, y0), (x1, y1), (x0, y1), (x0, y0)],
+                       color="red")
+        imgS.draw_line([(x0+1, y0+1), (x1-1, y0+1), (x1-1, y1-1),
+                        (x0+1, y1-1), (x0+1, y0+1)], color="white")
+        imgS.save(self.work_dir + 'input_0s.png')
+        # crop the image
+        # try cropping from the original input image
+        # (if different from input_1)
+        im0 = image(self.work_dir + 'input_0.orig.png')
+        dx0 = im0.size[0]
+        img = image(self.work_dir + 'input_0.png')
+        dx = img.size[0]
+        if dx != dx0:
+            z = float(dx0)/float(dx)
+            im0.crop((int(x0*z), int(y0*z), int(x1*z), int(y1*z)))
+            # resize if cropped image is too big
+            if (self.input_max_pixels
+                and prod(im0.size) > self.input_max_pixels):
+                im0.resize(self.input_max_pixels, method="antialias")
+            img = im0
+        else:
+            img.crop((x0, y0, x1, y1))
+        # save result
+        img.save(self.work_dir + 'input_0.sel.png')
+        return
+
+
+    @cherrypy.expose
+    @init_app
+    def rectangle(self, action=None, x=None, y=None, x0=None, y0=None, \
+                  *args, **kwargs):
+        """
+        params handling
+        """
+        if action == 'run':
+            for key in kwargs:
+                self.cfg['param'][key] = kwargs[key]
+            self.cfg.save()
+ 
+            if x != None:
+                 #save parameters
+                 try:
+                     for key in kwargs:
+                         self.cfg['param'][key] = kwargs[key]
+                     self.cfg['param']['x0'] = x0
+                     self.cfg['param']['y0'] = y0
+                     self.cfg['param']['x1'] = x
+                     self.cfg['param']['y1'] = y
+                     self.cfg.save()
+                 except ValueError:
+                     return self.error(errcode='badparams',
+                                       errmsg="Incorrect parameters.")
+
+            # use the whole image if no subimage is available
+            try:
+                img = image(self.work_dir + 'input_0.sel.png')
+            except IOError:
+                img = image(self.work_dir + 'input_0.png')
+                img.save(self.work_dir + 'input_0.sel.png')
+
+            # go to the wait page, with the key
+            http.redir_303(self.base_url + "wait?key=%s" % ((self.key)))
+            return
+        else:
+            # use a part of the image
+            if x0 == None:
+                # first corner selection
+                x = int(x)
+                y = int(y)
+                # draw a cross at the first corner
+                img = image(self.work_dir + 'input_0.png')
+                img.draw_cross((x, y), size=4, color="white")
+                img.draw_cross((x, y), size=2, color="red")
+                img.save(self.work_dir + 'input.png')
+                return self.tmpl_out("params.html", x0=x, y0=y, **kwargs)
+            else:
+                # second corner selection
+                x0 = int(x0)
+                y0 = int(y0)
+                x1 = int(x)
+                y1 = int(y)
+                # reorder the corners
+                (x0, x1) = (min(x0, x1), max(x0, x1))
+                (y0, y1) = (min(y0, y1), max(y0, y1))
+                assert (x1 - x0) > 0
+                assert (y1 - y0) > 0
+                #save parameters
+                try:
+                    for key in kwargs:
+                        self.cfg['param'][key] = kwargs[key]
+                    self.cfg['param']['x0'] = x0
+                    self.cfg['param']['y0'] = y0
+                    self.cfg['param']['x1'] = x
+                    self.cfg['param']['y1'] = y
+                    self.cfg.save()
+                except ValueError:
+                    return self.error(errcode='badparams',
+                                      errmsg="Incorrect parameters.")
+                #select subimage
+                self.select_subimage(x0, y0, x1, y1)
+                # go to the wait page, with the key
+                http.redir_303(self.base_url + "wait?key=%s" % ((self.key)))
+            return
+
+
     #
     # ARCHIVE
     #
