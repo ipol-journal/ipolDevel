d80ba37982f7128b5ca57d7dd4be130adf40b4c9
Alexis <alexis.mongin@outlook.com>
2015-09-29 08:02:41 +0200

    [Control terminal] Added terminal utilities, changed automatic detection of modules to XML parsing

--- ipol_demo/modules/controller.py
+++ ipol_demo/modules/controller.py
@@ -19,6 +19,7 @@ This script implement a terminal for controlling the IPOL system.
 import os
 import subprocess
 import urllib
+import xml.etree.ElementTree as ET
 
 class Controller(object):
     """
@@ -29,21 +30,33 @@ class Controller(object):
     def get_dict_modules():
         """
         Return a dictionary of the differents IPOL modules as keys, and
-        their respectives URL at which the services can be accessed as values.
+        another dictionary as value, containing two keys : a url (string), and
+        a list of strings representing the commands availables to the module.
         """
         dict_modules = {}
-        os.system("ls -d */ > .ctrl_tmp")
+        tree = ET.parse('modules.xml')
+        root = tree.getroot()
 
-        with open(".ctrl_tmp") as f:
-            list_modules = f.readlines()
+        for module in root.findall('module'):
+            dict_tmp = {}
+            list_tmp = []
 
-        list_modules = [i.strip('/\n') for i in list_modules]
+            for command in module.findall('command'):
+                list_tmp.append(command.text)
 
-        for module in list_modules:
-            with open(module + "/module_url") as f:
-                dict_modules[module] = f.readline()
+            list_tmp.append("info")
+            dict_tmp["url"] = module.find('url').text
+            dict_tmp["commands"] = list_tmp
+            dict_modules[module.get('name')] = dict_tmp
 
         return dict_modules
+
+    @staticmethod
+    def do_nothing():
+        """
+        Do nothing :)
+        """
+        return
 
     def get_actives_modules(self):
         """
@@ -53,7 +66,7 @@ class Controller(object):
 
         for key, value in self.dict_modules.items():
             try:
-                netobjct = urllib.urlopen(value + "ping")
+                netobjct = urllib.urlopen(value["url"] + "ping")
                 active_modules.append(key)
             except IOError:
                 pass
@@ -68,50 +81,42 @@ class Controller(object):
         """
         Ping specified module.
         """
-
-        if module in self.dict_modules.keys():
-
-            if module not in self.active_modules:
-                print "Module not active."
-                try:
-                    netobjct = urllib.urlopen(self.dict_modules[module]
-                                              + "ping")
-                    print "But he's responsing ! Something went really wrong."
-                    self.actives_modules.append(module)
-                except IOError:
-                    pass
-            else:
-                try:
-                    netobjct = urllib.urlopen(self.dict_modules[module]
-                                              + "ping")
-                    print "Module active, he say pong !"
-                except IOError:
-                    print "Module active, unresponsive."
-
+        if module not in self.active_modules:
+            print "Module not active."
+            try:
+                netobjct = urllib.urlopen(self.dict_modules[module]["url"]
+                                          + "ping")
+                print "But he's responsing ! Something went really wrong."
+                self.actives_modules.append(module)
+            except IOError:
+                pass
         else:
-            print "module " + module + " don't exist."
+            try:
+                netobjct = urllib.urlopen(self.dict_modules[module]["url"]
+                                          + "ping")
+                print "Module active, he say pong !"
+            except IOError:
+                print ("Module active, unresponsive. " +
+                       "(check your connection)")
 
     def shutdown_module(self, module):
         """
         Shutdown specified module.
         """
-        if module in self.dict_modules.keys():
-            try:
-                netobjct = urllib.urlopen(self.dict_modules[module]
-                                          + "shutdown")
-                self.active_modules.remove(module)
-                print module + " shut down."
-            except IOError:
-                pass
-                print "Service unreachable."
-        else:
-            print "module " + module + " don't exist."
-
+        try:
+            netobjct = urllib.urlopen(self.dict_modules[module]["url"]
+                                      + "shutdown")
+            self.active_modules.remove(module)
+            print module + " shut down."
+        except IOError:
+            pass
+            print "Service unreachable."
+            
     def start_module(self, module):
         """
         Start specified module.
         """
-        try :
+        try:
             if module not in self.active_modules:
                 os.chdir(module)
                 subprocess.Popen(["python", "main.py", module + "conf"],
@@ -132,16 +137,72 @@ class Controller(object):
         self.shutdown_module(module)
         self.start_module(module)
 
+    def info_module(self, module):
+        """
+        Give user info on specified module.
+        """
+        if module in self.active_modules:
+            status = "active."
+        else:
+            status = "unactive."
+
+        print "Status of " + module + " module : " + status
+        print "List of avalaible commands for " + module + " module :"
+        for command in self.dict_modules[module]["commands"]:
+            if command != "info":
+                print "    " + command
+        print ""
+
+    def process_command(self, tab_input, entry_buffer):
+        """
+        Check if the input is valid by verifying if the given module
+        exist, and if the command given is authorized for the given module.
+        """
+        if tab_input[1] in self.dict_modules.keys():
+
+            if tab_input[0] in self.dict_modules[tab_input[1]]["commands"]:
+                entry_buffer[tab_input[0]](tab_input[1])
+            else:
+                print ("Requested command isn't available for module " +
+                       tab_input[1])
+
+        else:
+            print "Module " + tab_input[1] + " don't exist."
+
+    def display_modules(self):
+        """
+        Display the modules.
+        """
+        for module in self.dict_modules.keys():
+            if module in self.active_modules:
+                print module + " : active"
+            else:
+                print module + " : unactive"
+
+    def display_help(self):
+        """
+        Help of the terminal
+        """
+        print "Read the freaking manual" # temporary
+
     def exec_loop(self):
         """
         Execution loop of the terminal.
         """
         str_input = str()
-        entry_buffer = {
+        entry_buffer_modules = {
             "start" : self.start_module,
             "shutdown" : self.shutdown_module,
             "restart" : self.restart_module,
-            "ping" : self.ping_module
+            "ping" : self.ping_module,
+            "info" : self.info_module
+        }
+        entry_buffer_terminal = {
+            "exit" : self.do_nothing,
+            "" : self.do_nothing,
+            "modules": self.display_modules,
+            "module": self.display_modules,
+            "help" : self.display_help
         }
 
         print "Welcome to the Ipol control terminal."
@@ -157,21 +218,25 @@ class Controller(object):
                 str_input = raw_input("admin@IPOL:~>")
                 tab_input = str_input.split(" ", -1)
 
-                if tab_input[0] != "exit" and tab_input[0] != "":
-
-                    if len(tab_input) < 2 :
+                if tab_input[0] in entry_buffer_terminal.keys():
+                    entry_buffer_terminal[tab_input[0]]()
+                elif tab_input[0] in entry_buffer_modules.keys():
+                    if len(tab_input) < 2:
                         print "Invalid command."
-                    elif tab_input[0] in entry_buffer.keys():
-                        entry_buffer[tab_input[0]](tab_input[1])
                     else:
-                        print tab_input[0] + " is not a command."
-  
+                        self.process_command(tab_input, entry_buffer_modules)
+                else:
+                    print "Invalid command."
+
         except EOFError:
             str_input = "exit"
             print
             pass
 
 def main():
+    """
+    Main function of the program.
+    """
     terminal = Controller()
     terminal.exec_loop()
 
