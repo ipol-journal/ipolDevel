ff02892508c87206d51a7a1bef009982fdcee95f
nmonzonlo <nmonzonlo@gmail.com>
2015-11-27 19:42:25 +0100

    [modules/proxy] The proxy shows the modules that appears in xml file

---
+++ ipol_demo/modules/proxy/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+
+"""
+Main function.
+"""
+
+import cherrypy
+import sys
+from module import Proxy
+
+if __name__ == '__main__':
+    cherrypy.quickstart(Proxy(None), config="proxy.conf")
