b10560b4a28d2bb7a870404bc4ee031e38b917fb
Karl Krissian <krissian@dis.ulpgc.es>
2016-05-12 09:48:38 +0200

    [static/JQueryApp] added zoom and display all to ImageGallery class, and removed 'style' parameter

--- ipol_demo/static/JQueryApp/JQueryGallery/jquery_gallery.js
+++ ipol_demo/static/JQueryApp/JQueryGallery/jquery_gallery.js
@@ -13,6 +13,7 @@ var ImageGallery = function(galleryid)  {
     this.contents = {};
     this.ref_index = 0;
     this.galleryid = galleryid;
+    this.img_class = 'gallery_'+this.galleryid+'_img';
     this.style="height:200px;";
     this.hover_bgcolor='rgba(255,228,196,155)';
     this.ref_bgcolor  ='#BCD2EE';
@@ -20,18 +21,25 @@ var ImageGallery = function(galleryid)  {
     this.onload_callback    = undefined;
     this.ondisplay_callback = undefined;
     this.onloadall_callback = undefined;
+    // set maximum display dimensions
+    this.display_maxwidth  = $(window).width()*0.80;
+    this.display_maxheight = $(window).height()*0.80;
+    console.info("max dimensions = ",this.display_maxwidth, "x",this.display_maxheight);
+    this.display_minwidth  = 400;
+    this.display_minheight = 300;
+    this.scales=[0.125,0.25, 0.333, 0.5, 0.667, 0.75,1, 1.5, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
     
     //--------------------------------------------------------------------------
     this.Append = function(content) {
         $.extend(this.contents,content);
     }
     
-    //--------------------------------------------------------------------------
-    this.SetStyle = function(s) {
-        console.info("Gallery style is ", s);
-        this.style=s;
-    }
-
+//     //--------------------------------------------------------------------------
+//     this.SetStyle = function(s) {
+//         console.info("Gallery style is ", s);
+//         this.style=s;
+//     }
+// 
     //--------------------------------------------------------------------------
     this.SetOnLoad = function( callback ) {
         this.onload_callback = callback;
@@ -49,17 +57,16 @@ var ImageGallery = function(galleryid)  {
 
     //--------------------------------------------------------------------------
     this.CreateZoomSelection = function() {
-        var scales=[0.125,0.25,0.5,0.75,1, 1.5, 2, 3, 4, 5, 6 , 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
         var res='';
         this.zoom_id = 'gallery_'+this.galleryid+'_zoomfactor';
         res += "x&nbsp;<select id='"+this.zoom_id+"'>";
-            for(var id=0;id<scales.length;id++) {
-                if (scales[id]==this.ZoomFactor) {
+            for(var id=0;id<this.scales.length;id++) {
+                if (this.scales[id]==this.ZoomFactor) {
                     res += "<option selected='selected'>";
                 } else {
                     res += "<option>";
                 }
-                res += scales[id]+"</option>";
+                res += this.scales[id]+"</option>";
             }
         res += "</select>";
         return res;
@@ -82,11 +89,6 @@ var ImageGallery = function(galleryid)  {
         html +=   "<td style='vertical-align:top;"+title_style+"'>";
 
             html += "<table style='"+title_style+"'>";
-//             html += "<tr style='"+title_style+"'>";
-//             html +=   "<td style='border:0;height:2em' >";
-//             html +=   this.CreateZoomSelection();
-//             html +=   "</td>";
-//             html += "</tr>";
             $.each(titles, function(index,title) {
                 html += "<tr style='"+title_style+"'>";
                 html +=   "<td id='t"+index+"' style='white-space:nowrap;border:0;height:1.25em;' >";
@@ -95,6 +97,16 @@ var ImageGallery = function(galleryid)  {
                 html += "</tr>";
             }.bind(this)
             );
+            html += "<tr style='"+title_style+"'>";
+            html +=   "<td style='border:0;height:2em' >";
+            html +=   this.CreateZoomSelection();
+            html +=   "</td>";
+            html += "</tr>";
+            html += "<tr style='"+title_style+"'>";
+            html +=   "<td style='border:0;height:2em' >";
+            html +=   "<button id='popup_all'>all</button>"
+            html +=   "</td>";
+            html += "</tr>";
             html += "</table>";
 
         html +=   "</td>";
@@ -104,7 +116,26 @@ var ImageGallery = function(galleryid)  {
         html += "</tr>";
             
         html += "</table>";
+        
+        html += "<div id='gallery_"+this.galleryid+"_all' title='Gallery Images' ></div>";
+        
+//     // parameters description dialog
+//     var param_desc_dialog;
+//     param_desc_dialog = $("#ParamDescription").dialog({
+//       autoOpen: false,
+// //       height: 500,
+//       width: 800,
+//       modal: true,
+//     });
+//     
+//     $("#description_params").button().on("click", 
+//         function() 
+//         { 
+//             param_desc_dialog.dialog("open");
+//         });
         return html;
+        
+        
     };
         
     
@@ -116,9 +147,9 @@ var ImageGallery = function(galleryid)  {
         var image = this.contents[title];
         console.info("CreateContents ", title, ": ", image);
         var res="";
-        this.img_class = 'gallery_'+this.galleryid+'_img';
 
-        var img_style = this.style;
+        var img_style = "";
+        //this.style;
         
         // string case
         switch ($.type(image)){
@@ -171,41 +202,93 @@ var ImageGallery = function(galleryid)  {
         }
     }
     
+    
+    //--------------------------------------------------------------------------
+    this.CheckLoadAllImages = function(index) {
+        if (this.total_loaded_images===this.total_images) {
+            console.info("All images are loaded ... running callback ");
+            if (this.onloadall_callback!=undefined) {
+                this.onloadall_callback();
+            }
+            // adapt zoom factor based on maximum display allowed
+            var ratio = 1;
+            ratio = Math.min(ratio,this.display_maxheight/this.max_height);
+            ratio = Math.min(ratio,this.display_maxwidth/this.max_width);
+            console.info("ratio = ",ratio);
+            if (ratio<1) {
+                // find scale to select
+                var idx=0;
+                while (this.scales[idx]<ratio) {
+                    idx++
+                }
+                if (idx>0) {
+                    idx--;
+                }
+                console.info("scale ",idx," = ",this.scales[idx]);
+                $("#"+this.zoom_id).val(this.scales[idx]);
+                $("#"+this.zoom_id).trigger("change");
+            } else {
+                // check min display constraint
+                // also risk of incompatibilities between min and max constraints
+                var ratio = 1;
+                ratio = Math.max(ratio,this.display_minheight/this.max_height);
+                ratio = Math.max(ratio,this.display_minwidth/this.max_width);
+                console.info("ratio = ",ratio);
+                if (ratio>1) {
+                    var idx=this.scales.length-1;
+                    while (this.scales[idx]>ratio) {
+                        idx--
+                    }
+                    if (idx<this.scales.length-1) {
+                        idx++;
+                    }
+                    // TODO: check max constraint is still OK
+                    console.info("scale ",idx," = ",this.scales[idx]);
+                    $("#"+this.zoom_id).val(this.scales[idx]);
+                    $("#"+this.zoom_id).trigger("change");
+                }
+            }
+            this.UpdateSelection();
+        }
+    }
+    
     //--------------------------------------------------------------------------
     this.CreateImages = function(index) {
         
         // need style ...
         var titles = Object.keys(this.contents);
         var image = this.contents[titles[index]];
-        console.info("CreateImages ", titles[index], ": ", image);
+        console.info("*** CreateImages ", titles[index], ": ", image);
         
         var res=[];
         
-        this.img_class = 'gallery_'+this.galleryid+'_img';
 
-        var img_style = this.style;
-        
+        // compute max width and height of all input images
+        this.max_height=10;
+        this.max_width=10;
+    
         var loading_font = {'font-style':'italic','font-weight':'bold'};
         var normal_font  = {'font-style':'normal','font-weight':'normal'};
         
         // string case
         switch ($.type(image)){
             case "string": 
+                console.info("string image");
                 var im = new Image();
                 this.total_images++;
                 res.push(im);
                 $(this.Elt(index)).css(loading_font);
                 im.onload = function() { 
+                    this.max_height = Math.max(this.max_height,im.height);
+                    this.max_width  = Math.max(this.max_width, im.width);
+                    console.info(" max :", this.max_width, "x" , this.max_height);
                     $(this.Elt(index)).css(normal_font);
                     console.info("image for ",titles[index], " loaded");
                     if (this.onload_callback!=undefined) {
                         this.onload_callback(index,im);
                     }
                     this.total_loaded_images++;
-                    if ((this.total_loaded_images===this.total_images)&&
-                        (this.onloadall_callback!=undefined)) {
-                        this.onloadall_callback();
-                    }
+                    this.CheckLoadAllImages();
                 }.bind(this);
                 im.src = image;
                 return res;
@@ -216,22 +299,23 @@ var ImageGallery = function(galleryid)  {
                     this.total_images++;
                     res.push(_im);
                     _im.onload = function() { 
+                        this.max_height = Math.max(this.max_height,_im.height);
+                        this.total_width[index] += _im.width;
+                        console.info(" max :", this.max_width, "x" , this.max_height);
                         //$(this.Elt(index)).css('font-style','normal');
                         console.info("[",l,"] for ",titles[index], " loaded");
                         this.nb_loaded_images[index]++;
                         // check if all images are loaded
                         if (this.nb_loaded_images[index]==Object.keys(image).length) {
                             console.info("all images loaded for ",titles[index]);
+                            this.max_width  = Math.max(this.max_width, this.total_width[index]);
                             $(this.Elt(index)).css(normal_font);
                         }
                         if (this.onload_callback!=undefined) {
                             this.onload_callback(index,_im);
                         }
                         this.total_loaded_images++;
-                        if ((this.total_loaded_images===this.total_images)&&
-                            (this.onloadall_callback!=undefined)) {
-                            this.onloadall_callback();
-                        }
+                        this.CheckLoadAllImages();
                     }.bind(this);
                     _im.src = im;
                 }.bind(this) );
@@ -243,19 +327,20 @@ var ImageGallery = function(galleryid)  {
                     this.total_images++;
                     res.push(_im);
                     _im.onload = function() { 
+                        this.max_height = Math.max(this.max_height,_im.height);
+                        this.total_width[index] += _im.width;
+                        console.info(" max :", this.max_width, "x" , this.max_height);
                         //$(this.Elt(index)).css('font-style','normal');
                         console.info("image ",idx," for ",titles[index], " loaded");
                         this.nb_loaded_images[index]++;
                         // check if all images are loaded
                         if (this.nb_loaded_images[index]==image.length) {
                             console.info("all images loaded for ",titles[index]);
+                            this.max_width  = Math.max(this.max_width, this.total_width[index]);
                             $(this.Elt(index)).css(normal_font);
                         }
                         this.total_loaded_images++;
-                        if ((this.total_loaded_images===this.total_images)&&
-                            (this.onloadall_callback!=undefined)) {
-                            this.onloadall_callback();
-                        }
+                        this.CheckLoadAllImages();
                     }.bind(this);
                     _im.src = im;
                 }.bind(this) );
@@ -282,6 +367,7 @@ var ImageGallery = function(galleryid)  {
     //--------------------------------------------------------------------------
     this.SetContents = function(index) {
         $(this.SelContents()).html(this.html_contents[index]);
+        $("."+this.img_class).css("height",(this.ZoomFactor*this.max_height)+"px");
         if (this.ondisplay_callback!=undefined) {
             this.ondisplay_callback(index);
         }
@@ -323,6 +409,10 @@ var ImageGallery = function(galleryid)  {
         // count loaded images to know when all images of a tab are loaded
         this.nb_loaded_images = Array(titles.length);
         for (var i = 0; i < titles.length; i++) this.nb_loaded_images[i] = 0; 
+
+        // compute total width for array or object elements
+        this.total_width = Array(titles.length);
+        for (var i = 0; i < titles.length; i++) this.total_width[i] = 0; 
 
         // count total number of loaded images
         this.total_loaded_images = 0;
@@ -372,12 +462,61 @@ var ImageGallery = function(galleryid)  {
                 }.bind(this) ); 
         }.bind(this));
         
-//         $("#"+this.zoom_id).change( 
-//             function() {
-//                 this.ZoomFactor = $("#"+this.zoom_id+" option:selected").val();
-//                 console.info(" new ZoomFactor = ", this.ZoomFactor);
-//             }.bind(this)
-//         );
+        $("#"+this.zoom_id).change( 
+            function() {
+                this.ZoomFactor = $("#"+this.zoom_id+" option:selected").val();
+                console.info(this.zoom_id+ " changed ", this.ZoomFactor);
+                this.UpdateSelection();
+            }.bind(this)
+        );
+        
+        
+        $("#gallery_"+this.galleryid+" #popup_all").button().unbind().on("click", 
+            function() 
+            { 
+                // parameters description dialog
+//                     width: 'auto',
+//                     height: 'auto',
+//                     maxWidth: $(window).width()*0.95,
+//                     maxHeight: $(window).height()*0.95,
+//                     position: [30,30],
+                    //{ my: "center", at: "center", of: window },
+                this.gallery_dialog = $("#gallery_"+this.galleryid+"_all").dialog({
+                    autoOpen: false,
+                    width: $(window).width()*0.95,
+                    height: $(window).height()*0.95,
+                    modal: true,
+                    close: function(event, ui) {
+                        $(this).empty().dialog('destroy');
+                    }
+                });
+        
+                console.info("popup_all click length=",this.html_contents.length);
+                var html = "<div >";
+                for(var i=0;i<this.html_contents.length;i++) {
+                    html += '<div style="display:inline-block;vertical-align: top;">';
+                    html += "<table>";
+                    html +=   "<tr><td>";
+                    html +=   this.html_contents[i];
+                    html +=   "</td></tr>";
+                    html +=   "<tr><td style='text-align:center;'>";
+                    html +=   titles[i];
+                    html +=   "</td></tr>";
+                    html += "</table>";
+                    html += "</div>";
+                }
+                html += "</div>";
+                console.info("setting ","#gallery_"+this.galleryid+"_all", " to ", html);
+                this.gallery_dialog.dialog("open");
+                $("#gallery_"+this.galleryid+"_all").html(html);
+                $("."+this.img_class).css("height",(this.ZoomFactor*this.max_height)+"px");
+                if (this.ondisplay_callback!=undefined) {
+                    for(var i=0;i<this.html_contents.length;i++) {
+                        this.ondisplay_callback(i);
+                    }
+                }
+            }.bind(this));
+        
     }
     
 }
