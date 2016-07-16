b323605af544dbaaeb4c77bee500a145a307ec13
Karl Krissian <krissian@dis.ulpgc.es>
2016-07-16 14:22:55 +0200

    [static/JQueryApp] renamed files

--- ipol_demo/static/JQueryApp/ipol_servers.js
+++
@@ -1,7 +0,0 @@
-
-// global variable
-var servers = {
-    proxy :"http://ns3018037.ip-151-80-24.eu:9003/",
-    // TODO: allow post with the proxy for uploads
-    demorunner :  "http://ns3018037.ip-151-80-24.eu:9004/"
-};
