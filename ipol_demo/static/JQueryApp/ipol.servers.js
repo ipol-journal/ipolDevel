753b9ecd59b36fc9a06771ef6e15b28deff96bbc
Miguel Colom <miguel@mcolom.info>
2016-07-17 18:17:48 +0200

    [JQuery] Changed ipol.server.js to point to ipolcore.ipol.im

--- ipol_demo/static/JQueryApp/ipol.servers.js
+++ ipol_demo/static/JQueryApp/ipol.servers.js
@@ -2,12 +2,8 @@
 // global variable
 var servers = {
     proxy :
-//                "http://127.0.0.1:9003/",
-//    proxy :
-//                 "http://10.0.2.2:9003/",
-             "http://ns3018037.ip-151-80-24.eu:9003/",
+             "http://ipolcore.ipol.im:9003/",
     // TODO: allow post with the proxy for uploads
     demorunner : 
-             "http://ns3018037.ip-151-80-24.eu:9004/"
-//            "http://127.0.0.1:9004/"
+             "http://ipolcore.ipol.im:9004/"
 };
