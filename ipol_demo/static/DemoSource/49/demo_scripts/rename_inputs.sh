9a4b67770240115f1256ed10f535d4c5ad331a42
Karl Krissian <krissian@dis.ulpgc.es>
2016-07-30 16:17:24 +0200

    [static/DemoSource/49/demo_script] improved renaming script

--- ipol_demo/static/DemoSource/49/demo_scripts/rename_inputs.sh
+++ ipol_demo/static/DemoSource/49/demo_scripts/rename_inputs.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 
 # create symbolic links for all input_XX.png files as iiXX.png
-for f in  `ls input_?.png`; do ln -s  $f ii`echo $f | cut -b 7-` ; done
+
+for f in  `ls input_?.png`;  do ln -s  $f ii`echo $f | cut -b 7-` ; done
+if [ -f input_10.png ]
+then
+  for f in  `ls input_??.png`; do ln -s  $f ii`echo $f | cut -b 7-` ; done
+fi
