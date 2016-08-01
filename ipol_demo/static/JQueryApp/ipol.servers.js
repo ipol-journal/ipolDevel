254e880b666f910086d4f2c2e1076d95a936dda5
nmonzonlo <nmonzonlo@gmail.com>
2016-08-01 12:26:27 +0100

    [static/JQueryApp] ipol.servers correction

--- ipol_demo/static/JQueryApp/ipol.servers.js
+++ ipol_demo/static/JQueryApp/ipol.servers.js
@@ -1,14 +1,14 @@
 
 // global variable
 var servers = {
-    proxy :
-                "http://127.0.0.1:9003/",
 //    proxy :
-//             "http://ns3018037.ip-151-80-24.eu:9003/",
+//                "http://127.0.0.1:9003/",
+    proxy :
+             "http://ns3018037.ip-151-80-24.eu:9003/",
     // TODO: allow post with the proxy for uploads
     demorunner_old : 
-//             "http://ns3018037.ip-151-80-24.eu:9006/"
-            "http://127.0.0.1:9006/"
+             "http://ns3018037.ip-151-80-24.eu:9006/"
+//            "http://127.0.0.1:9006/"
 };
 
 // switch between version using demorunner and version using core
