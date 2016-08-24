76424a0d16ea67cc56c9390d8aa45c073adda8c1
Karl Krissian <krissian@dis.ulpgc.es>
2016-08-24 09:49:53 +0200

    [static/DemoSource/49] added parameter to specify the reference image

--- ipol_demo/static/DemoSource/49/demo_scripts/rename_inputs.sh
+++ ipol_demo/static/DemoSource/49/demo_scripts/rename_inputs.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# create symbolic links for all input_XX.png files as iiXX.png
+# argument is the position of the reference image
+# switch its name with input_0.png
+ref=$1
+if [ $ref -ne 0 ] && [ -f input_$ref.png ]
+then
+  mv input_0.png tmp
+  mv input_$ref.png input_0.png
+  mv tmp input_$ref.png
+fi
 
+# create symbolic links for all input_XX.png files as iiXX.png
 for f in  `ls input_?.png`;  do ln -s  $f ii`echo $f | cut -b 7-` ; done
 if [ -f input_10.png ]
 then
   for f in  `ls input_??.png`; do ln -s  $f ii`echo $f | cut -b 7-` ; done
 fi
+
+
