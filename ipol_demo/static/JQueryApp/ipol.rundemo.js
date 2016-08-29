3a3f1efc2dcea951d66df5b1fd143d6f5720f660
Karl Krissian <krissian@dis.ulpgc.es>
2016-08-29 10:10:37 +0200

    [static/JQueryApp] getting rid of deprecated code and fixing issue #30

--- ipol_demo/static/JQueryApp/ipol.rundemo.js
+++ ipol_demo/static/JQueryApp/ipol.rundemo.js
@@ -315,6 +315,11 @@ ipol.RunDemo = function(ddl_json,input_origin, crop_info, blobset,
             new_state["upload_state"] = _getUploadPageState();
         }
         
+        if (_inpaint) {
+            var di = $("#DrawInputs").data("draw_inputs");
+            new_state["mask_state"] = di.getInpaint().getState();
+        }
+        
         try {
             // change url hash
             History.pushState(
@@ -326,168 +331,6 @@ ipol.RunDemo = function(ddl_json,input_origin, crop_info, blobset,
             _priorityMessage("error:", err.message);
         }
     }
-    
-    //--------------------------------------------------------------------------
-    /**
-     * runs the demo once the input(s) are selected (and cropped) or uploaded
-     * @function _doRun
-     * @memberOf ipol.RunDemo~
-     * @param {object} res result obtained from upload or select and crop
-     * calls to demorunner_old
-     * @private
-     * @fires demorunner_old:run_demo
-     * @fires archive:add_experiment
-     * @fires History.pushState
-     */
-     var _doRun = function(res) {
-        
-        _priorityMessage("upload/select&crop res=", res);
-        
-        if (res.status==="KO") {
-            _progress_info = "upload/select&crop:failure";
-            _progress(100);
-            return;
-        } else {
-            // reset progress after build
-            //_progress(0);
-        }
-        // create parameters
-        if (_ddl_json.params) {
-            var params = ipol.DrawParams.staticGetParamValues(_ddl_json.params);
-        } else {
-            var params = {};
-        }
-        // add crop info as parameters (would only need image size...)
-        params['x0']=Math.round(_crop_info.x);
-        params['x1']=Math.round(_crop_info.x+_crop_info.w);
-        params['y0']=Math.round(_crop_info.y);
-        params['y1']=Math.round(_crop_info.y+_crop_info.h);
-        
-        // if drawing line segments, add them as parameters
-        if (_drawlines) {
-            _drawlines.AddLinesParameters(params);
-        }
-        
-        _infoMessage("params = ",params);
-        // create meta information
-        var meta={};
-        if (res["process_inputs_msg"]!=undefined) {
-            _infoMessage("adding message to meta data ");
-            meta["process_inputs_msg"] = res["process_inputs_msg"];
-        }
-        meta["max_width"]  = res["max_width"];
-        meta["max_height"] = res["max_height"];
-        meta["original"]   = (_input_origin==="localfiles")
-        
-        _progress_info = "run_demo";
-        
-        // run_demo needs inputs, config and run from ddl_json
-        var ddl_json_parts = {};
-        ddl_json_parts['inputs']  = _ddl_json.inputs;
-        ddl_json_parts['config']  = _ddl_json.config;
-        ddl_json_parts['run']     = _ddl_json.run;
-        ddl_json_parts['archive'] = _ddl_json.archive;
-        // sending the result section seems problematic some some demos 
-        // (like optical flow demos for example)
-        
-        // run demo
-        var url_params = "demo_id="+_ddl_json.demo_id+
-                    "&key="+res.key+
-                    "&ddl_json="+_json2Uri(ddl_json_parts)+
-                    "&params=" +_json2Uri(params)+
-                    "&meta=" +_json2Uri(meta);
-        ipol.utils.ModuleService("demorunner_old","run_demo",url_params,
-            function(run_demo_res) {
-                _resultProgress(run_demo_res);
-                if ((run_demo_res.status==="KO")&&
-                    (!_ddl_json.general['show_results_on_error'])) {
-                        return;
-                }
-                _priorityMessage("run_demo run_demo_res=", run_demo_res);
-
-                // push history state will trigger result drawing ...
-                // Set url state for browser history
-                var new_state={
-                    demo_id       : _ddl_json.demo_id,
-                    state         : 2,
-                    res           : run_demo_res,
-                    ddl_json      : _ddl_json,
-                    scrolltop     : $(window).scrollTop(),
-                    crop_checked  : $("#id_cropinput").is(':checked')
-                };
-                // add blobset info if input if from the proposed blobsets
-                if (_blobset) {
-                    new_state["blobset"]      = _blobset;
-                } else {
-                    new_state["upload_state"] = _getUploadPageState();
-                }
-                
-                try {
-                    // change url hash
-                    History.pushState(
-                        new_state,
-                        "IPOL Journal - "+_ddl_json.general.demo_title,
-                        //"IPOLDemos "+_ddl_json.demo_id+" results",
-                        "?id="+_ddl_json.demo_id+"&res="+_json2Uri(run_demo_res));
-                } catch(err) {
-                    _priorityMessage("error:", err.message);
-                }
-                
-                // send to archive
-                if (run_demo_res.send_archive) {
-                    var url_params =    'demo_id='    + _ddl_json.demo_id + 
-                                        "&blobs="     + _json2Uri(run_demo_res.archive_blobs) + 
-                                        "&parameters="+ _json2Uri(run_demo_res.archive_params);
-                    ipol.utils.ModuleService("archive","add_experiment",url_params,
-                                  function(archive_res) {
-                                      console.info("archive add_experiment archive_res=",archive_res);
-                                  });
-                }
-            }
-        ).fail  ( function() {                             
-                    _progress_info = "failure";
-                    _progress(100); }
-                );
-    }    
-    
-    //--------------------------------------------------------------------------
-    /**
-     * Uploads the form that contains the input blobs and runs the demo 
-     * @function _uploadForm
-     * @memberOf ipol.RunDemo~
-     * @param {object} form_data contains the data to upload
-     * @private
-     * @fires demorunner_old:input_upload 
-     */
-    var _uploadForm = function( form_data) {
-        // We need to use ajax POST directly 
-        // with global variable servers.demorunner_old since
-        // it does not work with the proxy for the moment
-        form_data.append("module","demorunner_old");
-        form_data.append("service","input_upload");
-
-        // $.ajax(servers.demorunner_old+"input_upload",
-        $.ajax(servers.proxy+"proxy_post",
-        {
-            method: "POST",
-            data: form_data,
-            processData: false,
-            contentType: false,
-            //Do not cache the page
-            cache: false,
-            success: function ( res) {
-                _infoMessage('Upload success res=',res);
-                _progress_info = "upload success";
-                _doRun(JSON.parse(res));
-
-            },
-            error: function ( res) {
-                _infoMessage('Upload error res=',res);
-                _progress_info = "upload failure";
-                _progress(100);
-            }
-        });
-    };
     
     //--------------------------------------------------------------------------
     /**
@@ -531,140 +374,6 @@ ipol.RunDemo = function(ddl_json,input_origin, crop_info, blobset,
     /**
      * Sets the 'run' button click event, to call demorunner_old for upload,
      * select blobset with crop or initialize without inputs
-     * @function setRunEventOld
-     * @memberOf ipol.RunDemo~
-     * @public 
-     */
-    this.setRunEventOld = function() {
-        _initProgress();
-        
-        $( "#run_button" ).unbind("click").prop("disabled",false);
-        $( "#run_button" ).click( 
-        function(){
-            var ptext=_progresslabel.text();
-            // disable future clicks until run is finished
-            _progresslabel.text( "" );
-            _progress(0);
-            _progress_info = "initialization (check/build source code)";
-            var url_params =   "demo_id="+_ddl_json.demo_id+
-                            "&ddl_build="+_json2Uri(_ddl_json.build);
-            // code to be executed on click
-            ipol.utils.ModuleService("demorunner_old","init_demo", url_params,
-            function(res) {
-                console.info("init_demo res=", res);
-                if (res.status==="KO") {
-                    _progress_info = "init_demo:failure";
-                    _progress(100);
-                    return;
-                }
-                // select input from blobset or upload from local files
-                console.info("input_origin = ", _input_origin);
-                if (_inpaint) {
-                    _inpaint.submitInpaint(_ddl_json, _uploadForm);
-                } else {
-                    switch (_input_origin) {
-                        case "blobset":
-                            // need to deal with inpainting ...
-                            // Set inputs using blobset
-                            // crop at the same time
-                            _progress_info = "input selection and crop";
-                            url_params= "demo_id="+_ddl_json.demo_id+
-                                    "&ddl_inputs="+_json2Uri(_ddl_json.inputs)+
-                                    "&"+_blobset[0].html_params+
-                                    "&crop_info="+_json2Uri(_crop_info)
-                            ipol.utils.ModuleService("demorunner_old",
-                                                     "input_select_and_crop",
-                                                     url_params,
-                                                     _doRun
-                            ); // end input_select_and_crop
-                            break;
-
-                        case "localfiles":
-                            // upload files and run the demo
-                            _progress_info = "input upload";
-
-                            // fill form data to upload
-                            var formData = new FormData();
-                            formData.append('demo_id',    _ddl_json.demo_id);
-                            formData.append('ddl_inputs', JSON.stringify(_ddl_json.inputs));
-                            var inputs  = _ddl_json.inputs;
-                            if (inputs.length===1) {
-                                // Upload cropped image to server if the browser 
-                                // supports `HTMLCanvasElement.toBlob`
-                                var crop_enabled = $("#id_cropinput").is(':checked');
-                                if (crop_enabled) {
-                                    var cropped_canvas = $("#inputimage").cropper('getCroppedCanvas');
-                                    cropped_canvas.toBlob( 
-                                        function(blob) {
-                                            console.info('adding blob (cropped) : ', blob);
-                                            formData.append('file_0', blob);
-                                            _uploadForm(formData);
-                                        }, 'image/png' );
-                                } else {
-                                    var image_src = $("#inputimage").attr('src');
-                                    blobUtil.imgSrcToBlob(image_src).then(
-                                        function(blob) {
-                                            formData.append('file_0', blob);
-                                            _uploadForm(formData);
-                                        }, 'image/png' );
-                                }
-                            } else {
-                                // if several input image, TODO: deal with crop of first image
-                                var blobs_in_form=0;
-                                var image_src = [];
-                                var nb_uploads = 0;
-                                for(var idx=0;idx<inputs.length;idx++) {
-                                    var src = $('#localdata_preview_'+idx).attr("src");
-                                    // TODO: if image is not optional and src is undefined
-                                    // send error
-                                    if (src) {
-                                        image_src[idx] = src;
-                                        nb_uploads++;
-                                    }
-                                }
-                                
-                                for(var idx=0;idx<inputs.length;idx++) {
-                                    if (image_src[idx]) {
-                                        blobUtil.imgSrcToBlob(image_src[idx]).then(
-                                            function(idx) { return function(blob) {
-                                                console.info('idx=',idx);
-                                                formData.append('file_'+idx, blob);
-                                                blobs_in_form++;
-                                                console.info('blobs_in_form=',blobs_in_form);
-                                                if(blobs_in_form==nb_uploads) {
-                                                    _uploadForm(formData);
-                                                }
-                                            }
-                                            }(idx), 'image/png' );
-                                    }
-                                }
-                            }
-                            break;
-                            
-                        case "noinputs":
-                            // Set inputs using blobset
-                            // crop at the same time
-                            _progress_info = "initialize with no inputs";
-                            url_params= "demo_id="+_ddl_json.demo_id
-                            ipol.utils.ModuleService("demorunner_old",
-                                                     "init_noinputs",
-                                                     url_params,
-                                                     _doRun
-                            ); // end input_select_and_crop
-                            break;
-                    } // end switch input_origin
-                } // end if (inpaint)
-            }
-            ); // end init_demo
-        }
-        );
-    }
-
-    
-    //--------------------------------------------------------------------------
-    /**
-     * Sets the 'run' button click event, to call demorunner_old for upload,
-     * select blobset with crop or initialize without inputs
      * fill a FormData() variable with all the required information
      * to be able to run the demo in all the cases (blobset selection,
      * local file upload, no inputs, inpainting, etc...)
@@ -673,11 +382,11 @@ ipol.RunDemo = function(ddl_json,input_origin, crop_info, blobset,
      *  - blobset
      *  - upload
      *  - noinputs
-     * @function setRunEventNew
+     * @function setRunEvent
      * @memberOf ipol.RunDemo~
      * @public 
      */
-    this.setRunEventNew = function() {
+    this.setRunEvent = function() {
         _initProgress();
         
         $( "#run_button" ).unbind("click").prop("disabled",false);
@@ -798,22 +507,6 @@ ipol.RunDemo = function(ddl_json,input_origin, crop_info, blobset,
         );
     }
     
-    
-    //--------------------------------------------------------------------------
-    /**
-     * calls setRunEventOld or setRunEventNew based on the global variable
-     * use_core defined in ipol.servers.js
-     * @function setRunEvent
-     * @memberOf ipol.RunDemo~
-     * @public 
-     */
-    this.setRunEvent = function() {
-        if (use_core) {
-            this.setRunEventNew();
-        } else {
-            this.setRunEventOld();
-        }
-    }
 };
 
 
