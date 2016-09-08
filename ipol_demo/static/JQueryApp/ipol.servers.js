c189d8207ec495e1542c02098288538a41acc55f
Karl Krissian <krissian@dis.ulpgc.es>
2016-09-08 17:40:22 +0200

    [static/clientApp] renamed JQueryApp as clientApp

--- ipol_demo/static/JQueryApp/ipol.servers.js
+++
@@ -1,9 +0,0 @@
-
-// global variable
-var servers = {
-//    proxy :
-//                "http://127.0.0.1:9003/",
-    proxy :
-             "http://ns3018037.ip-151-80-24.eu:9003/",
-};
-
---
+++ ipol_demo/static/clientApp/ipol.servers.js
@@ -0,0 +1,8 @@
+
+// global variable
+var servers = {
+    proxy :
+                "http://127.0.0.1:9003/",
+//              "http://ns3018037.ip-151-80-24.eu:9003/",
+};
+
