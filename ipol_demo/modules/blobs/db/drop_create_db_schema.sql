a183de260951c2458d4ace5e0df4d477a9e6f18d
CarlosEscobar <carlosescobarblasco@gmail.com>
2016-09-28 14:42:34 +0200

    [modules/blobs] Creates a Data Base when it doesn't exists and improved created_db_squema file

--- ipol_demo/modules/blobs/db/drop_create_db_schema.sql
+++ ipol_demo/modules/blobs/db/drop_create_db_schema.sql
@@ -1,38 +1,37 @@
-CREATE TABLE "demo" (
+PRAGMA foreign_keys=ON;
+CREATE TABLE IF NOT EXISTS "demo" (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`name`	VARCHAR(30),
 	`is_template`	BOOLEAN DEFAULT 0,
 	`template_id`	INTEGER DEFAULT 0
 );
-CREATE TABLE "blob" (
+CREATE TABLE IF NOT EXISTS "blob" (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`hash`	VARCHAR(70),
+	`hash`	VARCHAR(70) NOT NULL,
 	`format`	VARCHAR(30),
-	`extension`	VARCHAR(30) DEFAULT '.png',
+	`extension`	VARCHAR(30),
 	`title`	VARCHAR(255),
 	`credit`	LONGTEXT
 );
-CREATE TABLE "tag" (
+CREATE TABLE IF NOT EXISTS "tag" (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-	`name`	VARCHAR(70)
+	`name`	VARCHAR(70) NOT NULL
 );
-CREATE TABLE  "blob_tag" (
+CREATE TABLE IF NOT EXISTS "blob_tag" (
 	`blob_id`	INTEGER NOT NULL,
 	`tag_id`	INTEGER NOT NULL,
 	PRIMARY KEY(blob_id,tag_id),
 	FOREIGN KEY(`blob_id`) REFERENCES "blob" ( `id` ) ON DELETE CASCADE ON UPDATE RESTRICT,
 	FOREIGN KEY(`tag_id`) REFERENCES "tag" ( `id` ) ON DELETE CASCADE ON UPDATE RESTRICT
 );
-CREATE INDEX `fk_blob_has_tag_tag1_idx` ON `blob_tag` (`tag_id` ASC);
-CREATE INDEX `fk_blob_has_tag_blob1_idx` ON `blob_tag` (`blob_id` ASC);
-CREATE TABLE CREATE TABLE "demo_blob" (
+
+CREATE TABLE IF NOT EXISTS "demo_blob" (
+	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`blob_id`	INTEGER NOT NULL,
 	`demo_id`	INTEGER NOT NULL,
 	`blob_set`	VARCHAR(70) DEFAULT NULL,
-	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`blob_pos_in_set`	INTEGER,
 	FOREIGN KEY(`blob_id`) REFERENCES "blob" ( `id` ) ON DELETE CASCADE ON UPDATE RESTRICT,
 	FOREIGN KEY(`demo_id`) REFERENCES "demo" ( `id` ) ON DELETE CASCADE ON UPDATE RESTRICT
 );
-CREATE INDEX fk_blob_has_demo_demo1_idx ON demo_blob (demo_id ASC);
-CREATE INDEX fk_blob_has_demo_blob1_idx ON demo_blob (blob_id ASC);
+
