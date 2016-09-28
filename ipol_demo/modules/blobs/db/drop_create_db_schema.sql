31f86be0c51877cca7427aab9a01eb251f2696ee
CarlosEscobar <carlosescobarblasco@gmail.com>
2016-09-28 10:52:34 +0200

    [modules/archive, blobs] Updated create_db_squema files

--- ipol_demo/modules/blobs/db/drop_create_db_schema.sql
+++ ipol_demo/modules/blobs/db/drop_create_db_schema.sql
@@ -16,18 +16,18 @@ CREATE TABLE "tag" (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
 	`name`	VARCHAR(70)
 );
-CREATE TABLE "blob_tag" (
-	`blob_id`	INTEGER,
-	`tag_id`	INTEGER,
+CREATE TABLE  "blob_tag" (
+	`blob_id`	INTEGER NOT NULL,
+	`tag_id`	INTEGER NOT NULL,
 	PRIMARY KEY(blob_id,tag_id),
 	FOREIGN KEY(`blob_id`) REFERENCES "blob" ( `id` ) ON DELETE CASCADE ON UPDATE RESTRICT,
 	FOREIGN KEY(`tag_id`) REFERENCES "tag" ( `id` ) ON DELETE CASCADE ON UPDATE RESTRICT
 );
 CREATE INDEX `fk_blob_has_tag_tag1_idx` ON `blob_tag` (`tag_id` ASC);
 CREATE INDEX `fk_blob_has_tag_blob1_idx` ON `blob_tag` (`blob_id` ASC);
-CREATE TABLE "demo_blob" (
-	`blob_id`	INTEGER,
-	`demo_id`	INTEGER,
+CREATE TABLE CREATE TABLE "demo_blob" (
+	`blob_id`	INTEGER NOT NULL,
+	`demo_id`	INTEGER NOT NULL,
 	`blob_set`	VARCHAR(70) DEFAULT NULL,
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`blob_pos_in_set`	INTEGER,
