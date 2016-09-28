41c478d1e70956e53a03ee2361e205190170d3a6
Miguel Colom <miguel@mcolom.info>
2016-09-28 18:48:32 +0200

    [sysadmin] Updated cleanup.sh with the shared_folder path

--- sysadmin/cleanup.sh
+++ sysadmin/cleanup.sh
@@ -3,5 +3,4 @@
 # Very simple script to clean temporal run directories
 # Miguel Colom, 2016
 
-find "/home/ipol/ipolDevel/ipol_demo/modules/demorunner/run/" -maxdepth 3 -ctime +30 -path '*/tmp/*' -type d -exec rm -rf {} \;
-
+find "/home/ipol/ipolDevel/shared_folder/run/" -maxdepth 2 -ctime +30 -type d -exec rm -rf {} \;
