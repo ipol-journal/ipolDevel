034dfdc683ac3956e61c15971ff35567bea28416
Karl Krissian <krissian@dis.ulpgc.es>
2016-09-30 17:48:16 +0200

    [modules/core/static/clientApp] added in_production flag in servers global object to remove demo selector and DDL display

--- ipol_demo/modules/core/static/clientApp/ipol.servers.js
+++ ipol_demo/modules/core/static/clientApp/ipol.servers.js
@@ -4,5 +4,7 @@ var servers = {
     proxy :
 //              "http://127.0.1.1:9003/",
               "http://ns3018037.ip-151-80-24.eu:9003/",
+    // this flag will remove the DDL display and demo selector
+    in_production : true
 };
 
