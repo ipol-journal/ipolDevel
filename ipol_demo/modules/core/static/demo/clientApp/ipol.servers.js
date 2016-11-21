d2821a87d1a0060e7c064ba3b2be42becc6220f6
CarlosEscobar <carlosescobarblasco@gmail.com>
2016-11-21 17:22:40 +0100

    [modules/core] Fixed bad proxy server in ipol.servers.js

--- ipol_demo/modules/core/static/demo/clientApp/ipol.servers.js
+++ ipol_demo/modules/core/static/demo/clientApp/ipol.servers.js
@@ -2,8 +2,7 @@
 // global variable
 var servers = {
     proxy :
-              "http://127.0.1.1:9003/",
-//              "http://ns3018037.ip-151-80-24.eu:9003/",
+              "http://ns3018037.ip-151-80-24.eu:9003/",
     // this flag will remove the DDL display and demo selector
     in_production : true
 };
