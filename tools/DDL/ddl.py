04adbabb6d072cfd519e77665ad46c2007807e8c
CarlosEscobar <carlosescobarblasco@gmail.com>
2016-12-09 16:44:00 +0100

    [tools/DDL] Finished ddl.py

--- tools/DDL/ddl.py
+++ tools/DDL/ddl.py
@@ -18,38 +18,110 @@
 
 import optparse
 import sys
+import requests
+import json
+import os
+
+HOST = "integration.ipol.im"
+
+
+def post(service, params=None, json=None):
+    try:
+        url = 'http://{}/api/demoinfo/{}'.format(
+            HOST,
+            service
+        )
+        # print "JSON - ",json
+        return requests.post(url, params=params, json=json)
+    except Exception as ex:
+        print "ERROR: Failure in the post function - {}".format(str(ex))
+
 
 def do_read(args):
     '''
     Read a DDL
     '''
-    print "read"
-    print args
+    for editorsdemoid in args:
+        try:
+            resp = post('read_last_demodescription_from_demo', params={"demo_id": editorsdemoid, "returnjsons": True})
+            response = resp.json()
+            if response['status'] != 'OK':
+                print "ERROR: read_last_demodescription_from_demo returned KO for demo {}".format(editorsdemoid)
+                continue
+            last_demodescription = response['last_demodescription']
+            ddl_json = json.loads(json.loads(last_demodescription['json']))
 
-def do_write(args):
-    '''
-    Write a DDL
-    '''
-    print "write"
-    print args
-    
+            file = open("DDLs/" + str(editorsdemoid) + ".json", "wb")
+            file.write(json.dumps(ddl_json, indent=4, sort_keys=True))
+            file.close()
+        except Exception as ex:
+            print "ERROR: Failed to read DDL from {} - {}".format(editorsdemoid, ex)
+
+
 def do_read_all():
     '''
+    Read all DDLs
+    '''
+    resp = post('demo_list')
+    response = resp.json()
+    if response['status'] != 'OK':
+        print "ERROR: demo_list returned KO"
+        return
+    demos = []
+    for demo in response['demo_list']:
+        demos.append(demo['editorsdemoid'])
+    do_read(demos)
+
+
+def do_write(demos):
+    '''
     Write a DDL
     '''
-    print "do_read_all"
+    for editorsdemoid in demos:
+        try:
+            file = open("DDLs/" + str(editorsdemoid) + ".json", "r").read()
+            # Check if is a valid JSON
+            json.loads(file)
+
+            resp = post('save_demo_description', params={"demoid": editorsdemoid}, json=file)
+            response = resp.json()
+            if response['status'] != 'OK':
+                print "ERROR: save_demo_description returned KO for demo {}".format(editorsdemoid)
+        except ValueError:
+            print "ERROR: Invalid JSON for demo {}".format(editorsdemoid)
+        except Exception as ex:
+            print "ERROR: Could not write DDL for demo {} - {}".format(editorsdemoid, ex)
+
+def do_write_all():
+    '''
+    Write all DDLs
+    '''
+    resp = post('demo_list')
+    response = resp.json()
+    if response['status'] != 'OK':
+        print "ERROR: demo_list returned KO"
+        return
+
+    demos = []
+    for demo in response['demo_list']:
+        demos.append(demo['editorsdemoid'])
+    do_write(demos)
+
 
 # Parse program arguments
 parser = optparse.OptionParser()
 (opts, args) = parser.parse_args()
 
 if len(args) < 1:
-    print "Error: no command specified!\n"
+    print "ERROR: no command specified!\n"
     parser.print_help()
     sys.exit(-1)
 
+if not os.path.isdir("DDLs"):
+    os.mkdir("DDLs")
+
 command = args[0].lower()
-print command
+# print command
 
 if command == 'readall' or command == 'getall':
     do_read_all()
@@ -57,6 +129,7 @@ elif command == 'read' or command == 'get':
     do_read(args[1:])
 elif command == 'write' or command == 'put':
     do_write(args[1:])
+# elif command == 'writeall' or command == 'putall':
+#     do_write_all()
 else:
     print "Unknown command '{}'".format(command)
-
