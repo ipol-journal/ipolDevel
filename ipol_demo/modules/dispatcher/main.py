80f82d2ced3215044075278c987d75f5fe2e0180
Miguel Colom <miguel@mcolom.info>
2017-01-08 00:55:14 +0100

    [modules/dispatcher] Reindent

--- ipol_demo/modules/dispatcher/main.py
+++ ipol_demo/modules/dispatcher/main.py
@@ -17,14 +17,13 @@ def CORS():
     cherrypy.response.headers["Access-Control-Allow-Origin"] = "*"
 
 if __name__ == '__main__':
-    
+
     cherrypy.tools.CORS = cherrypy.Tool('before_handler', CORS)
-    
+
     CONF_FILE_REL = sys.argv[1] if len(sys.argv) == 2 and os.path.isfile(sys.argv[1]) else "dispatcher.conf"
 
     BASE_DIR      = os.path.dirname(os.path.abspath(__file__))
     CONF_FILE_ABS = os.path.join(BASE_DIR, CONF_FILE_REL)
-    
+
     cherrypy.config.update(CONF_FILE_ABS)
     cherrypy.quickstart(Dispatcher(None,CONF_FILE_ABS), config=CONF_FILE_ABS)
-    
