750884a17594be69b79f29bb6d93cbb0d1d9393b
Martin Arevalo <martinarevalo8@gmail.com>
2017-03-01 18:23:16 +0100

    [CP, modules/demoinfo] Changed message in search bar. Moved default exception message from the module to the template. Changes in styles of Authors/Editors templates.

--- ipol_webapp/apps/controlpanel/views/demoinfo_module.py
+++ ipol_webapp/apps/controlpanel/views/demoinfo_module.py
@@ -164,8 +164,6 @@ class DemoinfoDemoEditionView(NavbarReusableMixinMF,TemplateView):
 
     def get_demo_details(self, **kwargs):
         data = {}
-        # message to send to template, when an exception is thrown
-        message = 'The query did not return any results. Please make sure that you entered a valid criteria.'
 
         try:
             try:
@@ -198,7 +196,6 @@ class DemoinfoDemoEditionView(NavbarReusableMixinMF,TemplateView):
 
         except Exception as ex:
             data['editorsdemoid'] = demo_id
-            data['message'] = message
             data['demoform'] = None
             data['status'] = 'KO'
 
