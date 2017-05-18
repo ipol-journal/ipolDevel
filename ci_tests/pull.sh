db5ea4d84d3454712c9f5117db7c2e691170ffa3
CarlosEscobar <carlosescobarblasco@gmail.com>
2017-05-18 11:19:56 +0200

    [test] Changed path to mails in pull.sh

--- ci_tests/pull.sh
+++ ci_tests/pull.sh
@@ -9,7 +9,7 @@ git pull
 
 function send_email () {
     msg="$*"
-    sendTo=$(cat /home/ipol/ipolDevel/sysadmin/email_tech.txt)
+    sendTo=$(cat /home/ipol/ipolDevel/ci_tests/send_to.txt)
     echo -e "This is the ${host} machine. The tests failed on ${today}:\n${msg}" | mutt -s "[${host}] Test failed" -- ${sendTo}
 }
 
