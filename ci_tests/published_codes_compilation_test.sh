6cab195311d4a533f75127849e9eabcafec07cc7
CarlosEscobar <carlosescobarblasco@gmail.com>
2017-05-25 12:01:09 +0200

    [ci_tests] Changed message in published_codes_compilation_test.sh

--- ci_tests/published_codes_compilation_test.sh
+++ ci_tests/published_codes_compilation_test.sh
@@ -2,12 +2,10 @@
 
 today=$(date)
 
-# Git pull
-
 function send_email () {
     msg="$*"
     sendTo=$(cat /home/ipol/ipolDevel/ci_tests/send_to.txt)
-    echo -e "This is the ${host} machine. The tests failed on ${today}:\n${msg}" | mutt -s "[${host}] Test failed" -- ${sendTo}
+    echo -e "This is the ${host} machine. The compilation tests failed on ${today}:\n${msg}" | mutt -s "[${host}] Compilation failed" -- ${sendTo}
 }
 
 out=$(python /home/ipol/ipolDevel/ci_tests/published_codes_compilation_test.py)
