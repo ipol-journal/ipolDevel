b8358ffecc747d72372fbd96082cad86ac2835f6
CarlosEscobar <carlosescobarblasco@gmail.com>
2017-06-14 17:30:12 +0200

    [sysadmin]Allow white spaces in the apt-get_requirements.txt

--- sysadmin/install_dist_packages/install_dist_packages.sh
+++ sysadmin/install_dist_packages/install_dist_packages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 while IFS='' read -r line || [[ -n "$line" ]]; do
+    # Remove commented lines
     [[ "$line" =~ ^#.*$ ]] && continue
-    echo "$line"
+    # Remove lines with spaces
+    [[ $line =~ ^' '*$ ]] && continue
+    # Remove empty lines
+    [[ "$line" == '' ]] && continue
     sudo apt-get -y install "$line"
 done < "apt-get_requirements.txt"
