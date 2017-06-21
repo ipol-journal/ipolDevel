2d41df5e2d0958f3685f2d41068f657e0f2355c4
Matías Abal Hevia <abalheviamatias@gmail.com>
2017-06-21 11:00:43 +0200

    [ClientApp2/Run] Run POST parameters added.

--- ipol_demo/clientApp/js/demo.run.js
+++ ipol_demo/clientApp/js/demo.run.js
@@ -2,69 +2,79 @@ var clientApp = clientApp || {};
 var helpers = clientApp.helpers || {};
 
 var errorMsg = "Error.";
+var runData = {};
 
 $(".run-btn").click(function() {
   runDemo();
 });
 
 function runDemo() {
+  runData = {};
+  setRunPostData();
+
+  // si no lo tiene 30. si lo tiene lo que especifique.
   $.ajax({
       url: '/api/core/run2',
       type: 'POST',
       dataType: 'json',
-      beforeSend: function(){
+      beforeSend: function() {
         hideRunningAnimation();
+        hideStatusContainer();
         displayRunningAnimation();
         return checkPostData();
       },
-      data: {
-        "demo_id": demo_id,
-        "params": params
-        // "origin":
-        // "crop_info"
-
-      }
+      data: runData
     })
     .done(function(res) {
       console.log(res);
-      displaySuccess("Success!");
+      if (res.status == "KO") displayError(res.error);
+      else displaySuccess("Excecution successful");
       //Call drawResults function
     })
     .fail(function(res) {
       console.log(res);
       displayError(res.responseText);
-      //Show back-end error messages
     })
-    .always(function() {
+    .always(function(res) {
       hideRunningAnimation();
       enableRunButton();
     });
 }
 
-function checkPostData(){
-  if(!checkPostInputs()) return false;
+function setRunPostData() {
+  var origin = helpers.getFromStorage("origin");
+  runData.demo_id = demo_id;
+  runData.params = params;
+  if (origin) runData.origin = origin;
+  if (origin == "blobSet") runData.blobs = helpers.getFromStorage("demoSet");
+  if (origin == "upload") runData.inputs = clientApp.upload.getUploadedFiles();
+  if ($("#crop-btn").is(":checked")) runData.crop_info = $("#editor-blob-left").cropper("getData");
+}
+
+function checkPostData() {
+  if (!checkPostInputs()) return false;
   return true;
 }
 
-function checkPostInputs() {
-  if(helpers.getFromStorage("demoInfo").inputs.length != 0 && !helpers.getFromStorage("origin")){
+function checkPostInputs()  {
+  if (helpers.getFromStorage("demoInfo").inputs.length != 0 && !helpers.getFromStorage("origin")) {
     errorMsg = "Input/s required."
     return false;
   }
   return true;
 }
 
-function enableRunButton(){
+function enableRunButton() {
   $(".run-btn").prop('disabled', false);
   $(".run-btn").css('background-color', '#5d8cb1');
 }
 
-function hideRunningAnimation(){
+function hideRunningAnimation() {
   $(".loader").addClass('di-none');
   $(".loader").removeClass('element-appear');
 }
 
-function hideStatusContainer(){
+function hideStatusContainer() {
   $(".run-msg-box").addClass('di-none');
 }
 
@@ -81,7 +91,7 @@ function displayError(msg) {
   $(".run-msg-box").addClass('element-appear');
   $(".run-msg-box").addClass('run-error');
 
-  $(".run-msg").html(msg || errorMsg);
+  $(".run-msg").html(msg || errorMsg);
 }
 
 function displaySuccess(msg) {
