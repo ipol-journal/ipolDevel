697193df754720390079987aab56adaaaae43559
CarlosEscobar <carlosescobarblasco@gmail.com>
2017-06-30 15:12:19 +0200

    [CP] Changed the way ddl history is shown

--- ipol_webapp/apps/controlpanel/controlpanel_static/js/manage_ddl_history.js
+++ ipol_webapp/apps/controlpanel/controlpanel_static/js/manage_ddl_history.js
@@ -1,16 +1,33 @@
+//Editor configuration
+editor = ace.edit("editor");
+editor.getSession().setMode("ace/mode/json");
+editor.getSession().setUseWrapMode(true);
+editor.getSession().setTabSize(4);
+editor.setAutoScrollEditorIntoView(true);
+
+editor.setOptions({
+    readOnly: true,
+    highlightActiveLine: false,
+    highlightGutterLine: false
+})
+editor.renderer.$cursorLayer.element.style.opacity=0
+
+selected_ddl = null
 
 function showDDL(ddl, creationDate){
-    ddl = ddl.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    document.getElementById("ddl").innerHTML=ddl;
+    selected_ddl=ddl
+    editor.setValue(ddl);
+    editor.gotoLine(0);
     document.getElementById("title").innerHTML="DDL from: "+creationDate;
 }
 
-function restoreDDL(url, ddl, demo_id){
+function restoreDDL(url, demo_id){
     var values = {
         'demo_id': demo_id,
-        'ddl': ddl,
+        'ddl': selected_ddl,
     }
     $.post(url, values, 'json');
     demo_url = "/cp/demo_edition/" + demo_id;
     window.location.href = demo_url;
 }
+
