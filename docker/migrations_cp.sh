506f2d7f4266ac0046cdda223127e575e45aa576
Héctor Macías Casado <hectormaciasc@gmail.com>
2017-09-25 17:23:09 +0200

    [Docker/zsh] Docker image uses zsh and added cp requirements install to build stage

--- docker/migrations_cp.sh
+++ docker/migrations_cp.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd /home/ipol/ipolDevel/ipol_webapp/ && \
-pip install  -r requirements.txt && \
 python manage.py makemigrations --empty controlpanel && \
 python manage.py makemigrations controlpanel && \
 python manage.py migrate controlpanel && \
