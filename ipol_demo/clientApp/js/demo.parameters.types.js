e4dc5c5400457abb5381dd2b6686ff29b5632ec5
Matías Abal Hevia <abalheviamatias@gmail.com>
2017-11-29 15:49:11 +0100

    [ClientApp/params] Check if default_value exists

--- ipol_demo/clientApp/js/demo.parameters.types.js
+++ ipol_demo/clientApp/js/demo.parameters.types.js
@@ -86,11 +86,11 @@ $.fn.numeric = function(param, index) {
 }
 
 $.fn.text = function(param, index) {
-  var values = param.values;
+  var default_text = param.values.default;
 
   $('.param-' + index).prepend('<span class=param-label >' + param.label + '</span>');
   $('<input id=text_' + param.id + ' class=range-slider__value type=text />').appendTo(this).addClass('input-text-param');
-  $("#text_" + param.id).val(param.values.default);
+  if(default_text) $("#text_" + param.id).val(default_text);
 
   $('#text_' + param.id).change(function(event) {
     updateParamsArrayValue(param.id, $('#text_' + param.id).val());
