ca955afe93c82c1f67a334203957587d02499100
Matías Abal Hevia <abalheviamatias@gmail.com>
2018-01-18 16:33:15 +0000

    [modules/DR] avoid that the “move” field copies from outside src or to ouside bin

--- ipol_demo/modules/demorunner/demorunner.py
+++ ipol_demo/modules/demorunner/demorunner.py
@@ -367,8 +367,16 @@ class DemoRunner(object):
                         # Remove possible white spaces
                         file_to_move = file_to_move.strip()
                         #
-                        path_from = os.path.join(src_dir, file_to_move)
-                        path_to = os.path.join(bin_dir, os.path.basename(file_to_move))
+
+                        path_from = os.path.realpath(os.path.join(src_dir, file_to_move))
+                        path_to = os.path.realpath(os.path.join(bin_dir, os.path.basename(file_to_move)))
+
+                        src_path = os.path.realpath(src_dir)
+                        bin_path = os.path.realpath(bin_dir)
+
+                        if not path_from.startswith(src_path) or not path_to.startswith(bin_path):
+                            raise IPOLUnauthorizedAccess(file_to_move)
+
 
                         # Check origin
                         if not os.path.exists(path_from):
@@ -472,6 +480,10 @@ format(str(ex), str(ddl_build)).encode('utf8')
             data = {}
             data['status'] = 'KO'
             data['message'] = "Construct failed. File not found. {}".format(str(ex))
+        except IPOLUnauthorizedAccess as ex:
+            data = {}
+            data['status'] = 'KO'
+            data['message'] = "Unauthorized access. Move: {}".format(str(ex))
         except urllib2.URLError as ex:
             data = {}
             data['status'] = 'KO'
