b391b3e160f349e72eb4d4d7083df0d0fb8321dd
aiis <nmonzonlo@gmail.com>
2018-02-05 16:31:57 +0100

    [modules/core] New exceptions in errors.py

--- ipol_demo/modules/core/errors.py
+++ ipol_demo/modules/core/errors.py
@@ -59,3 +59,83 @@ class IPOLMissingRequiredInputError(Exception):
     def __init__(self, index):
         super(IPOLMissingRequiredInputError, self).__init__()
         self.index = index
+
+
+class IPOLWorkDirError(Exception):
+    """
+    """
+    pass
+
+class IPOLKeyError(Exception):
+    """
+    """
+    pass
+
+
+class IPOLDecodeInterfaceRequestError(Exception):
+    """
+    """
+    pass
+
+
+class IPOLReadDDLError(Exception):
+    """
+    """
+    pass
+
+class IPOLFindSuitableDR(Exception):
+    """
+    """
+    pass
+
+
+
+class IPOLEnsureCompilationAndDemoExtrasError(Exception):
+    """
+    """
+    def __init__(self, interface_message, email_message=None):
+        super(IPOLPrepareFolderError, self).__init__()
+        self.interface_message = interface_message
+        self.email_message = email_message
+
+class IPOLConversionError(Exception):
+    """
+    """
+    pass
+
+class IPOLPrepareFolderError(Exception):
+    """
+    """
+    def __init__(self, interface_message, email_message=None):
+        super(IPOLPrepareFolderError, self).__init__()
+        self.interface_message = interface_message
+        self.email_message = email_message
+
+class IPOLExecutionError(Exception):
+    """
+    """
+    def __init__(self, interface_message, email_message=None):
+        super(IPOLPrepareFolderError, self).__init__()
+        self.interface_message = interface_message
+        self.email_message = email_message
+
+class IPOLError(Exception):
+    """
+    """
+    pass
+
+class IPOLDemoRunnerResponseError(Exception):
+    """
+    """
+    def __init__(self, message, demo_state, key, error):
+        super(IPOLDemoRunnerResponseError, self).__init__()
+        self.message = message
+        self.demo_state = demo_state
+        self.key = key
+        self.error = error
+
+        
+class IPOLArchiveError(Exception):
+    """
+    """
+    pass
