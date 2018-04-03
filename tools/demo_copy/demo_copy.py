6640c0cf7ce430f5726efd49d4682b7b4edae732
Héctor Macías Casado <hectormaciasc@gmail.com>
2018-04-03 11:07:33 +0200

    [tools] Refactor to add -i flag

--- tools/demo_copy/demo_copy.py
+++ tools/demo_copy/demo_copy.py
@@ -25,21 +25,21 @@ import socket
 import urllib.request
 
 
-def post(module, service, host, params=None, data=None, files=None, servicejson=None):
+def post(service, host, params=None, data=None, files=None, servicejson=None):
     """
     Post request to service
     """
-    url = 'http://{}/api/{}/{}'.format(host, module, service)
+    url = 'http://{}{}'.format(host, service)
     return requests.post(url, params=params, data=data, files=files, json=servicejson)
 
-def get(module, service, host, params=None):
+def get(service, host, params=None):
     """
     Get resource
     """
-    url = 'http://{}/api/{}/{}/{}'.format(host, module, service, params)
+    url = 'http://{}{}/{}'.format(host, service, params)
     return urllib.request.urlopen(url)
     
-def copy_demo(demo_id, host):
+def copy_demo(demo_id, host, source_host):
     """
     Copy demo main method.
     """
@@ -71,16 +71,16 @@ def copy_demo(demo_id, host):
     add = add_ddl(host, new_id)
     os.remove('ddl.json')
     print('Adding demo_extras...')    
-    clone_demo_extras(host, demo_id, new_id)
+    clone_demo_extras(host, source_host, demo_id, new_id)
     print('Adding blobs...')    
-    copy_blobs_to_demo(host, demo_id, new_id)
+    copy_blobs_to_demo(host, source_host, demo_id, new_id)
     
     demo_templates = get_demo_template_names(source_host, demo_id)
     local_templates = get_all_templates(host)
     for template_name in demo_templates:
         if template_name not in local_templates:
             print('Cloning template: {}...'.format(template_name))
-            copy_template(host, demo_id, new_id, template_name)
+            copy_template(host, source_host, demo_id, new_id, template_name)
             associate_template(host, new_id, template_name)
         else:
             print('Associating template: {}...'.format(template_name))
@@ -91,7 +91,7 @@ def ddl_exists(demo_id, host):
     """
     Check if DDL exists
     """
-    http_response = post("demoinfo", 'get_ddl', host, params={"demo_id": demo_id}).json()
+    http_response = post('/api/demoinfo/get_ddl', host, params={"demo_id": demo_id}).json()
     if http_response['status'] == 'OK':
         return True
     return False
@@ -100,7 +100,7 @@ def get_demo_state(host, demo_id):
     """
     Get a demo state
     """
-    response = get('demoinfo', 'read_demo_metainfo', host, demo_id)
+    response = get('/api/demoinfo/read_demo_metainfo', host, demo_id)
     string = response.read().decode('utf-8')
     json_obj = json.loads(string)
     return json_obj['state']
@@ -110,7 +110,7 @@ def get_ddl(demo_id, host):
     Read a DDL
     """
     try:
-        resp = post("demoinfo", 'get_ddl', host, params={"demo_id": demo_id})
+        resp = post('/api/demoinfo/get_ddl', host, params={"demo_id": demo_id})
         response = resp.json()
         if response['status'] != 'OK':
             message = "ERROR: get_ddl returned KO for demo {} from {}".format(demo_id, host)
@@ -140,22 +140,22 @@ def add_ddl(host, demo_id):
     with open('ddl.json', 'r') as out:
         ddl = out.read()
 
-    response = post('demoinfo', 'save_ddl', host, params=params, data=ddl)
+    response = post('/api/demoinfo/save_ddl', host, params=params, data=ddl)
     return response.json()
 
 def create_demo(demo_id, title, state, host):
     """
     Creates a demo
     """
-    response = post('demoinfo', 'add_demo', host, params={'editorsdemoid': demo_id, 'title': title, 'state': state})
+    response = post('/api/demoinfo/add_demo', host, params={'editorsdemoid': demo_id, 'title': title, 'state': state})
     return response.json()
 
-def clone_demo_extras(host, demo_id, new_id):
+def clone_demo_extras(host, source_host, demo_id, new_id):
     """
     Get DemoExtras from a demo
     """
     serviceparams = {'demo_id': demo_id}
-    response = post('demoinfo', 'get_demo_extras_info', source_host, params=serviceparams).json()
+    response = post('/api/demoinfo/get_demo_extras_info', source_host, params=serviceparams).json()
     if 'url' in response:
         url = response['url']
         filename = response['url'].split("/")[-1]
@@ -168,15 +168,15 @@ def add_demo_extras(host, demo_id, demo_extras, demo_extras_name):
     """
     files = {'demoextras': demo_extras}
     params = {'demo_id': demo_id, 'demoextras_name': demo_extras_name}
-    response = post('demoinfo', 'add_demoextras', host, params=params, files=files)
+    response = post('/api/demoinfo/add_demoextras', host, params=params, files=files)
     return response.json()
 
-def copy_blobs_to_demo(host, demo_id, new_id):
+def copy_blobs_to_demo(host, source_host, demo_id, new_id):
     """
     Copy blobs to demo or template
     """
     params = {'demo_id': demo_id}
-    blobs_json = post('blobs', 'get_demo_owned_blobs', source_host, params=params).json()
+    blobs_json = post('/api/blobs/get_demo_owned_blobs', source_host, params=params).json()
     local_demo_blobs = get_demo_owned_blobs(host, new_id)
     for blobset in blobs_json['sets']:
         set_name = blobset['name']
@@ -199,7 +199,7 @@ def get_demo_owned_blobs(host, demo_id):
     Get all blobs owned by a demo
     """
     params = {'demo_id': demo_id}
-    blobs_response = post('blobs', 'get_demo_owned_blobs', host, params=params).json()
+    blobs_response = post('/api/blobs/get_demo_owned_blobs', host, params=params).json()
     owned_blobs = []
     for blobset in blobs_response['sets']:
         blobs = blobset['blobs']
@@ -225,7 +225,7 @@ def add_blob_to_demo(host, demo_id, blob, blob_title, set_name, pos_in_set, cred
     if vr:
         files['blob_vr'] = vr
     params = {'demo_id': demo_id, 'title': blob_title, 'blob_set': set_name, 'pos_set': pos_in_set, 'credit': credit}
-    response = post('blobs', 'add_blob_to_demo', host, params=params, files=files)
+    response = post('/api/blobs/add_blob_to_demo', host, params=params, files=files)
     return response.json()
 
 #####################
@@ -235,7 +235,7 @@ def get_all_templates(host):
     """
     Get all existing templates from a host
     """
-    response = post('blobs', 'get_all_templates', host).json()
+    response = post('/api/blobs/get_all_templates', host).json()
     return response['templates']
 
 def get_demo_template_names(host, demo_id):
@@ -243,7 +243,7 @@ def get_demo_template_names(host, demo_id):
     Get all templates associated to a demo
     """
     params = {'demo_id': demo_id}
-    response = post('blobs', 'get_demo_templates', host, params=params).json()
+    response = post('/api/blobs/get_demo_templates', host, params=params).json()
     return response['templates']
 
 def associate_template(host, new_id, template_name):
@@ -251,22 +251,22 @@ def associate_template(host, new_id, template_name):
     Associate template to a demo
     """
     params = {'demo_id': new_id, 'template_names': template_name}
-    response = post('blobs', 'add_templates_to_demo', host, params=params)
+    response = post('/api/blobs/add_templates_to_demo', host, params=params)
 
-def copy_template(host, demo_id, new_id, template_name):
+def copy_template(host, source_host, demo_id, new_id, template_name):
     """
     Copy template to local
     """
     params = {'template_name': template_name}
-    response = post('blobs', 'create_template', host, params=params).json()
-    copy_blobs_from_template(host, template_name)
+    response = post('/api/blobs/create_template', host, params=params).json()
+    copy_blobs_from_template(host, source_host, template_name)
 
-def copy_blobs_from_template(host, template_name):
+def copy_blobs_from_template(host, source_host, template_name):
     """
     Copy blobs to template
     """
     params = {'template_name': template_name}
-    blobs_json = post('blobs', 'get_template_blobs', source_host, params=params).json()
+    blobs_json = post('/api/blobs/get_template_blobs', source_host, params=params).json()
     # blobs_json = blobs_response.json()
 
     for blobset in blobs_json['sets']:
@@ -292,7 +292,7 @@ def add_blob_to_template(host, template_name, blob, set_name, pos_in_set, credit
     if vr:
         files['blob_vr'] = vr
     params = {'template_name': template_name, 'title': title, 'blob_set': set_name, 'pos_set': pos_in_set, 'credit': credit}
-    response = post('blobs', 'add_blob_to_template', host, params=params, files=files)
+    response = post('/api/blobs/add_blob_to_template', host, params=params, files=files)
     return response.json()
 
 class Error(Exception):
@@ -312,19 +312,21 @@ class GetDDLError(Error):
 # Command help
 parser = argparse.ArgumentParser()
 parser.add_argument("demo_id", type=int, help="identifier of the demo to be copied")
-
+parser.add_argument("-i", '--integration', help="Use the Integration environment", action="store_true")
 args = parser.parse_args()
-
-# Host destination of the demo
-host = socket.getfqdn()
-if (host != "integration.ipol.im"):
-  host = "127.0.0.1"
-
-# Host origin of the demo
-source_host = "integration.ipol.im"
-# source_host = "ipolcore.ipol.im"
 
 # Obtain demo ID from arguments
 demo_id = args.demo_id
+# Source host
+if args.integration:
+    source_host = "integration.ipol.im"
+else:
+    source_host = "ipolcore.ipol.im"
 
-copy_demo(demo_id, host)
+print('Source: ', source_host)
+# Host destination of the demo
+host = socket.getfqdn()
+if (host != "integration.ipol.im" and host != "ipolcore.ipol.im"):
+  host = "127.0.0.1"
+
+copy_demo(demo_id, host, source_host)
