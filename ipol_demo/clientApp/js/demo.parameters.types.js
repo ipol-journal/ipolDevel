29a989e7655ce6b111fc9c7769d195090f728326
Matías Abal Hevia <abalheviamatias@gmail.com>
2018-05-15 16:38:53 +0200

    [ClientApp/parameteres] Selection radio display block

--- ipol_demo/clientApp/js/demo.parameters.types.js
+++ ipol_demo/clientApp/js/demo.parameters.types.js
@@ -42,8 +42,9 @@ $.fn.selection_radio = function(param, index) {
   $('.param-' + index).prepend('<span class=param-label >' + label + '</span>');
   $(this).append('<div id=param-' + param.id + '></div>');
   for (var i = 0; i < keys.length; i++) {
-    $('#param-' + param.id).append('<input class=hand type=radio name=' + param.id + ' id=' + param.id + '_' + i + ' value=' + values[i] + '>');
-    $('#param-' + param.id).append('<label class=hand for=' + param.id + '_' + i + ' >' + keys[i] + '</label>');
+    $('#param-' + param.id).append('<div class="radio-option-' + i + '"></div>')
+    $('#param-' + param.id + " > .radio-option-" + i).append('<input class=hand type=radio name=' + param.id + ' id=' + param.id + '_' + i + ' value=' + values[i] + '>')
+    $('#param-' + param.id + " > .radio-option-" + i).append('<label class=hand for=' + param.id + '_' + i + ' >' + keys[i] + '</label>');
     if (param.default_value == values[i]) $('#' + param.id + '_' + i).prop('checked', true);
   }
   $('#param-' + param.id + ' > label').addClass('m-r-10');
