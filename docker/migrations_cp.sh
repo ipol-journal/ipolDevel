323cec89136fbe2972105722d9af88665e6f2e88
Héctor Macías <hectormaciasc@gmail.com>
2018-06-26 10:14:04 +0200

    [docker] Migrations for the cp2 added. Cp2 alias created.

--- docker/migrations_cp.sh
+++ docker/migrations_cp.sh
@@ -5,3 +5,8 @@ python manage.py makemigrations controlpanel && \
 python manage.py migrate controlpanel && \
 python manage.py migrate && \
 python manage.py collectstatic
+
+cd /home/ipol/ipolDevel/cp2 && \
+python3 -m virtualenv cp2 && \
+pip3 install -r requirements.txt && \
+cd ControlPanel && python3 manage.py migrate && deactivate
