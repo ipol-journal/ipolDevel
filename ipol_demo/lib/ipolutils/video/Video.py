60e6901dd90df0b17bc45495547b205e6b65a6cc
Miguel Colom <miguel@mcolom.info>
2018-07-02 16:13:13 +0200

    [lib/video] Review corrections

--- ipol_demo/lib/ipolutils/video/Video.py
+++ ipol_demo/lib/ipolutils/video/Video.py
@@ -94,7 +94,7 @@ class Video(object):
         avconv_command += self.get_avconv_options(max_pixels, max_frames)
         avconv_command += " -loglevel error " + processed_video
 
-        # shlex can be useful when determining the correct tokenization for args, especially in complex cases
+        # Tokenike args, to prevent shell-injection attacks
         convert_proc = Popen(shlex.split(avconv_command))
         convert_proc.wait()
 
