390435bb6e2a0cfb03eeb9b2c21b0e29a2e1eef6
Matías Abal Hevia <abalheviamatias@gmail.com>
2018-09-14 18:06:45 +0200

    [modules/core] Raise exception instead of return check_ddl

--- ipol_demo/modules/core/core.py
+++ ipol_demo/modules/core/core.py
@@ -199,7 +199,7 @@ class Core(object):
 
             self.load_demorunners()
         except Exception as ex:
-            self.logger.exception("__init__", str(ex))
+            self.logger.exception("__init__")
 
     def read_authorized_patterns(self):
         """
@@ -780,8 +780,8 @@ class Core(object):
                         if required_field in fields_positive:
                             value = evaluate(input_in_ddl[required_field])
                             integer = float(value) == int(value)
-                            if not (value > 0 and integer):
-                                return "Bad DDL inputs section: '{}' field must be a positive integer value in input #{}.".format(required_field, inputs_counter)
+                            if value <= 0 or not integer:
+                                raise IPOLCheckDDLError("Bad DDL inputs section: '{}' field must be a positive integer value in input #{}.".format(required_field, inputs_counter))
 
         # The params must be a list
         if 'archive' in ddl and 'params' in ddl['archive'] and not isinstance(ddl['archive']['params'], list):
