7a31f0bbc194048da5457799ae0142491dc6cabd
Miguel Colom <miguel@mcolom.info>
2018-10-01 10:23:48 +0200

    [sysadmin/cleanup] Remove demo directories without any executions

--- sysadmin/cleanup.sh
+++ sysadmin/cleanup.sh
@@ -2,5 +2,9 @@
 
 # Very simple script to clean temporal run directories
 # Miguel Colom, 2016
+BASE="/home/ipol/ipolDevel/shared_folder/run/"
 
-find "/home/ipol/ipolDevel/shared_folder/run/" -maxdepth 2 -ctime +30 -type d -exec rm -rf {} \;
+find ${BASE} -maxdepth 2 -ctime +30 -type d -exec rm -rf {} \;
+
+# Remove demo directories without any executions
+find ${BASE} -maxdepth 1 -type d -empty -exec rm -rf {} \;
