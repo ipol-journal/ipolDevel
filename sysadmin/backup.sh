d12347f3d26be57254715a7161c893fc05d17e99
Héctor Macías <hectormaciasc@gmail.com>
2019-09-24 15:16:26 +0200

    [sysadmin] backup excludes shared folder and demo binaries

--- sysadmin/backup.sh
+++ sysadmin/backup.sh
@@ -3,8 +3,8 @@
 # Very simple script to make backups
 # Miguel Colom, 2016
 
-backupsFolder="/home/ipol/backups"
-folderToBackup="/home/ipol/ipolDevel"
+backupsFolder="~/backups"
+folderToBackup="~/ipolDevel"
 
 snapshot=$(date +"snapshot_%Y-%q")
 yearPeriod=$(date +"%Y-%q")
@@ -14,12 +14,14 @@ snapshotName=${backupsFolder}/${yearPeriod}/${snapshot}
 backupFilename=$(date +"backup_%m-%d-%Y.tgz")
 fullFilename=${backupsFolder}/${yearPeriod}/${backupFilename}
 
+demo_binaries="~/ipolDevel/ipol_demo/modules/demorunner/binaries"
+shared_folder="~/ipolDevel/shared_folder"
+
 mkdir -p ${backupsFolder}/${yearPeriod}
-tar -g $snapshotName -cpzf $fullFilename $folderToBackup
+tar -g $snapshotName -czf $fullFilename --exclude=$shared_folder --exclude=$demo_binaries $folderToBackup
 
 ##############
 # To restore a backup pipe a cat with all .tgz files into tar extract 
 # command and specify a destination path (restore_folder)
+# tar xvpzf tar_file1.tgz -g /dev/null --ignore-zeros -C restore_folder 
 ##############
-
-# cat *.tgz | tar xvfz - -g /dev/null --ignore-zeros -C restore_folder
