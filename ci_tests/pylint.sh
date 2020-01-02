ffded46ffea1a9d65fb80949402ef058e614dbe5
Héctor Macías <hectormaciasc@gmail.com>
2020-01-02 11:45:39 +0000

    [ci_tests] pylint script will create and run inside a virtualenv

--- ci_tests/pylint.sh
+++ ci_tests/pylint.sh
@@ -4,6 +4,7 @@
 # Miguel Colom, 2016
 
 me=$(whoami)
+ci_folder="/home/${me}/ipolDevel/ci_tests"
 modulesDir="/home/${me}/ipolDevel/ipol_demo/modules/"
 report="/home/${me}/ipolDevel/ci_tests/pylint_report.txt"
 modules="core archive blobs demoinfo demorunner dispatcher conversion"
@@ -16,13 +17,19 @@ echo >> ${report}
 # PyLint test on each IPOL module
 cur_dir=$(pwd)
 
+if [ ! -d "${ci_folder}/venv" ]; then
+    python3 -m virtualenv ${ci_folder}/venv
+    source ${ci_folder}/venv/bin/activate
+    pip install -r ${ci_folder}/requirements.txt
+fi
+
 for module in ${modules}
 do
     echo "**** MODULE: ${module}" >> ${report}
     cd ${modulesDir}${module}
     # Lint the module after activating its virtualenv
-    source venv/bin/activate
-    pylint --rcfile=/home/${me}/ipolDevel/ci_tests/pylintrc *.py >> ${report}
+    source ${ci_folder}/venv/bin/activate
+    pylint --rcfile=${ci_folder}/pylintrc *.py >> ${report}
     deactivate
     cd ${cur_dir}
 done
