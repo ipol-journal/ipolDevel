39c97eba81c14b0ccfac57694e77b32eec49e4e2
Miguel Colom <miguel@mcolom.info>
2021-08-16 12:02:48 +0200

    [sysadmin] check_shared_folder.sh: specify ipol's uid and gid when mounting, to avoid that it takes the permissions from a different user

--- sysadmin/check_shared_folder.sh
+++ sysadmin/check_shared_folder.sh
@@ -3,5 +3,5 @@
 sharedFolder="/home/ipol/ipolDevel/shared_folder"
 
 if [ $(mount | grep -c shared_folder) -eq 0 ];then
-	sshfs ipol_core:$sharedFolder $sharedFolder
+    sshfs ipol_core:$sharedFolder $sharedFolder -o uid=$(id -u ipol),gid=$(id -g ipol)
 fi
