2a2771d0119fb2c1cebf8bb79539e83e1c5bd2aa
Héctor Macías <hectormaciasc@gmail.com>
2022-02-14 12:11:54 +0000

    [clientApp] text parameters will try to set attribute values

--- ipol_demo/clientApp/js/demo.parameters.types.js
+++ ipol_demo/clientApp/js/demo.parameters.types.js
@@ -92,6 +92,7 @@ $.fn.text = function(param, index) {
   $('.param-' + index).prepend('<span class=param-label >' + param.label + '</span>');
   $('<input id=text_' + param.id + ' class=range-slider__value type=text />').appendTo(this).addClass('input-text-param');
   if(values && values.default) $("#text_" + param.id).val(values.default);
+  if(values) $("#text_" + param.id).attr(values);
 
   $('#text_' + param.id).change(function(event) {
     updateParamsArrayValue(param.id, $('#text_' + param.id).val());
