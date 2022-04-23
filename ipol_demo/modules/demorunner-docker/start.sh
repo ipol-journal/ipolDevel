bb8d33151fffeaabf89f930c2b45f5d626f2e608
Anger Jérémy <angerj.dev@gmail.com>
2022-04-23 10:34:40 +0200

    [modules] add demorunner-docker

---
+++ ipol_demo/modules/demorunner-docker/start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eux
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
+if [ -f $HOME/.cargo/env ]; then
+    source $HOME/.cargo/env
+fi
+
+if ! command -v cargo &> /dev/null; then
+    echo "cargo is required to launch demorunner-docker" >&2
+    echo "see https://rustup.rs/" >&2
+    exit 1
+fi
+
+cargo install --git https://github.com/kidanger/ipol-demorunner.git --branch master --root . --target-dir target --debug --force
+
+export ROCKET_PROFILE=ipol
+bin/ipol-demorunner >logs 2>&1 &
