fc15d82f3075afa7928f161191e2e7eccfe36535
Miguel Colom <miguel@mcolom.info>
2022-05-16 12:20:29 +0200

    [ci_tests/compilation] More robust published_codes_compilation_test.py

--- ci_tests/published_codes_compilation_test.py
+++ ci_tests/published_codes_compilation_test.py
@@ -26,7 +26,7 @@ def main():
         requirements = json_ddl.get('general').get('requirements')
 
         if build_section is None:
-            print(f"Demo #{demo_id} doesn't have build section")
+            print(f"Demo #{demo_id} doesn't have a build section")
             all_success = False
             continue
 
@@ -65,6 +65,10 @@ def get_published_demos():
     Get demos
     """
     resp = post(HOST, 'demoinfo', 'demo_list')
+    if resp == None:
+        print("Can't get the list of demos!")
+        return []
+
     response = resp.json()
     demos = []
     if response['status'] != 'OK':
@@ -81,32 +85,52 @@ def get_ddl(demo_id):
     Read the DDL of the demo
     """
     resp = post(HOST, 'demoinfo', 'get_ddl', params={"demo_id": demo_id})
+    if resp == None:
+        print(f"Can't get the DDL of demo #{demo_id}!")
+        return ""
+
     response = resp.json()
     if response['status'] != 'OK':
-        print(f"ERROR: get_ddl returned KO for demo {demo_id}")
+        print(f"ERROR: get_ddl returned KO for demo #{demo_id}")
     last_demodescription = response.get('last_demodescription')
     return last_demodescription.get('ddl')
 
 
 def build(demo_id, build_section, requirements, demorunners):
     """
-    Execute the build section. If any build fail the method returns a False
+    Execute the build section.
+    Returns False if any build fails. True is everything's fine.
     """
-    all_success = True
     suitable_demorunners = get_suitable_demorunners(requirements, demorunners)
     for demorunner in suitable_demorunners.keys():
         demorunner_host = demorunners[demorunner].get('server')
         params = {'ddl_build': json.dumps(build_section), 'compilation_path': get_compilation_path(demo_id)}
         response = post(demorunner_host, 'demorunner', 'test_compilation', params)
-        if response.status_code == 504:
-            all_success = False
-            print(f"Couldn't build demo #{demo_id}. Reason: DR={demorunner} gave error HTTP 504 (gateway timeout.")
-        else:
-            json_response = response.json()
-            if json_response.get('status') != 'OK':
-                all_success = False
+
+        if response == None or response.status_code == None:
+            print(f"Bad response from DR={demorunner} when trying to build demo #{demo_id}: {str(response)}")
+            return False
+
+        if response.status_code == 200:
+            json_response = response.json()            
+            if json_response.get('status') == 'OK':
+                continue
+            else:
                 print(f"Couldn't build demo #{demo_id} in DR={demorunner}.")
-    return all_success
+                return False
+        else:
+            msg = f"Bad HTTP response status_code from DR={demorunner} when trying to build demo #{demo_id}."
+            if response.status_code == 504:
+                msg += " HTTP error 504 (gateway timeout)"
+            elif response.status_code == 404:
+                msg += f" HTTP error 404 (not found)"
+            else:
+                msg += f" HTTP error {response.status_code}"
+            print(msg)
+            return False
+        
+    # No DR failed or KO: compilation test passed
+    return True
 
 
 def get_suitable_demorunners(requirements, demorunners):
@@ -164,7 +188,6 @@ def post(host, module, service, params=None):
         )
         return requests.post(url, params=params)
     except Exception as ex:
-        print(f"ERROR: Failure in the post function - {str(ex)}")
-
+        return None
 
 main()
