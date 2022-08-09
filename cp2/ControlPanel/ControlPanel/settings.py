f58203429df86ad018270312417126bbc5e62c64
Héctor Macías <hectormaciasc@gmail.com>
2022-08-09 15:16:37 +0100

    [cp2] email configuration fixed

--- cp2/ControlPanel/ControlPanel/settings.py
+++ cp2/ControlPanel/ControlPanel/settings.py
@@ -80,11 +80,12 @@ INSTALLED_APPS = [
     'gunicorn',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = HOST_NAME
-EMAIL_PORT = 2525
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'IPOL Team <noreply@ipol.com>'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
