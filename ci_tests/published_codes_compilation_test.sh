f5267b5859acbbbc9de1b2fe90f9d4bf44402fb5
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2022-08-30 12:26:14 +0530

    [ci_tests] corrected ci_tests directory path

--- ci_tests/published_codes_compilation_test.sh
+++ ci_tests/published_codes_compilation_test.sh
@@ -9,7 +9,7 @@ function send_email () {
     echo -e "This is the ${host} machine. The compilation tests failed on ${today}:\n${msg}" | mutt -s "[${host}] Compilation failed" -- ${sendTo}
 }
 
-ci_tests_dir=/home/jyotsna/ipolDevel/ci_tests
+ci_tests_dir=/home/ipol/ipolDevel/ci_tests
 
 out=$(python3 ${ci_tests_dir}/published_codes_compilation_test.py)
 if [ $? -ne 0 ];then
