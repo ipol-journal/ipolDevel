50f03247e92d816e3e6c85540a8cd41e3a3de2e1
Anger Jérémy <angerj.dev@gmail.com>
2022-09-02 18:22:42 +0200

    [cp2] add placeholders on login form

--- cp2/ControlPanel/ControlPanel/forms.py
+++ cp2/ControlPanel/ControlPanel/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class loginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
