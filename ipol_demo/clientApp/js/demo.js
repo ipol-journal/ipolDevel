c3b1eb97df4f60f8547664697ada212882a4888f
Héctor Macías <hectormaciasc@gmail.com>
2022-09-19 10:38:15 +0100

    [clientApp] demo tab will reload page preventing errors

--- ipol_demo/clientApp/js/demo.js
+++ ipol_demo/clientApp/js/demo.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     sessionStorage.clear();
     
     fetchData();
+    $("#demoTab")[0].href = 'demo.html?id=' + demo_id;
   } else
     showWrongDemoIdError();
 })
