9ebcc888d65e57d556aa26fa94bac4d835ff4da4
Anger Jérémy <angerj.dev@gmail.com>
2022-11-26 22:20:56 +0100

    [cp] remove ipol_webapp

--- ipol_webapp/apps/controlpanel/views/proxy_module.py
+++
@@ -1,2 +0,0 @@
-__author__ = 'josearrecio'
-# TODO NOT IMPLEMENTED YET, proxy could return stats apart from beeing the proxy, if yo need to access new proxy functionality, this should be oimplemented here,
--- ipol_webapp/vendor/Readme.txt
+++
@@ -1,2 +0,0 @@
-The apps directory stores all of your customized django apps
- and the vendor directory stores any apps you do not want to install (or can’t install) using pip or easy_install
