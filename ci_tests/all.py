fbea14094209a55ada6864894f411abf11e51c99
Anger Jérémy <angerj.dev@gmail.com>
2022-11-27 16:47:12 +0100

    [ci_tests] remove conversion

--- ci_tests/all.py
+++ ci_tests/all.py
@@ -14,13 +14,12 @@ archive_test = '/home/{}/ipolDevel/ipol_demo/modules/archive/test.py'.format(use
 blobs_test = '/home/{}/ipolDevel/ipol_demo/modules/blobs/test.py'.format(user)
 demoinfo_test = '/home/{}/ipolDevel/ipol_demo/modules/demoinfo/test.py'.format(user)
 demorunner_test = '/home/{}/ipolDevel/ipol_demo/modules/demorunner/test.py'.format(user)
-conversion_test = '/home/{}/ipolDevel/ipol_demo/modules/conversion/test.py'.format(user)
 
 resources = '/home/{}/ipolDevel/ci_tests/resources'.format(user)
 demorunners = '/home/{}/ipolDevel/ipol_demo/modules/config_common/demorunners.xml'.format(user)
 shared_folder = '/home/{}/ipolDevel/shared_folder'.format(user)
 
-tests = [demoinfo_test, blobs_test, archive_test, demorunner_test, conversion_test, system_test]
+tests = [demoinfo_test, blobs_test, archive_test, demorunner_test, system_test]
 
 def start():
     """
