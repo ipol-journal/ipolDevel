8559519eebb5a9f1f31c9a1e622a819a2874882c
Anger Jérémy <angerj.dev@gmail.com>
2023-01-07 22:08:40 +0100

    [demorunner-docker] update upstream changes

--- ipol_demo/modules/demorunner-docker/start.sh
+++ ipol_demo/modules/demorunner-docker/start.sh
@@ -15,7 +15,7 @@ if ! command -v cargo &> /dev/null; then
     exit 1
 fi
 
-cargo install --git https://github.com/ipol-journal/ipol-demorunner.git --rev 7bf38a4cb007ea1113ba6a08f133d3c217a405fa --root . --target-dir target --debug --force --locked
+cargo install --git https://github.com/ipol-journal/ipol-demorunner.git --rev cf4dce8a406580964dbea58a77b0d780b3878656 --root . --target-dir target --debug --force --locked
 
 export ROCKET_PROFILE=ipol-$(hostname)
 bin/ipol-demorunner >logs 2>&1 &
