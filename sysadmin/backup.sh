93228dde0af78136736862dc530bfc8eaf74ed98
Miguel Colom <miguel@mcolom.info>
2023-04-12 14:33:54 +0200

    [sysadmin] backup.sh fix. Also, don't manage the incremental copy here

--- sysadmin/backup.sh
+++ sysadmin/backup.sh
@@ -3,35 +3,21 @@
 # Very simple script to make backups
 # Miguel Colom, 2016
 
-backupsFolder="/home/ipol/backups"
-ipolDevelCopyPath=${backupsFolder}/ipolDevelCopy
-yearPeriod=$(date +"%Y-%q")
+user=ipol # Remember to set it back to "ipol" after testing
+modulesPath=/home/${user}/ipolDevel/ipol_demo/modules
+ipolDevelCopyPath=/home/${user}/backups/ipolDevelCopy
+ipolDevelCopyModulesPath=${ipolDevelCopyPath}/ipolDevel/ipol_demo/modules
 
-# Backup modules databases
-modulesWithDB=("archive" "blobs" "core")
-modulesPath="/home/ipol/ipolDevel/ipol_demo/modules/"
-for module in ${modulesWithDB[@]}; do
-  sqlite3 $modulesPath"$module"/db/$module.db ".backup '${modulesPath}${module}/db/${module}_backup.db'"
-done
-echo "Databases backed up"
-
-
+# Clone all
 mkdir -p ${ipolDevelCopyPath}
-rsync -azuP --delete --exclude 'shared_folder' /home/ipol/ipolDevel ${ipolDevelCopyPath}
+rsync -azuP --delete --exclude 'shared_folder' /home/${user}/ipolDevel ${ipolDevelCopyPath}
 echo "ipolDevel cloned to ${ipolDevelCopyPath}"
 
-snapshot=$(date +"snapshot_%Y-%q")
-snapshotName=${backupsFolder}/${yearPeriod}/${snapshot}
+# Export databases
+databases=("blobs/db/blobs.db" "core/db/demoinfo.db" "archive/db/archive.db")
 
-backupFilename=$(date +"backup_%m-%d-%Y.tgz")
-fullFilename=${backupsFolder}/${yearPeriod}/${backupFilename}
-
-mkdir -p $backupsFolder/$yearPeriod
-tar -g $snapshotName -czf $fullFilename $ipolDevelCopyPath
-echo "ipolDevel clone compressed into: $fullFilename"
-
-##############
-# To restore a backup pipe a cat with all .tgz files into tar extract 
-# command and specify a destination path (restore_folder)
-# tar xvpzf tar_file1.tgz -g /dev/null --ignore-zeros -C restore_folder 
-##############
+for database in ${databases[@]}; do
+  sqlite3 $modulesPath/${database} ".backup '${ipolDevelCopyModulesPath}/${database}'"
+  cp $modulesPath/${database} ${ipolDevelCopyModulesPath}/${database}.bak
+done
+echo "Databases backed up"
