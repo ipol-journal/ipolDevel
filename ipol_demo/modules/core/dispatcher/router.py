0afa9eff26ad552aada632b0889e36aec43df8ee
Héctor Macías <hectormaciasc@gmail.com>
2023-10-16 11:45:30 +0100

    [dispatcher] dispatcher class object added from router

--- ipol_demo/modules/core/dispatcher/router.py
+++ ipol_demo/modules/core/dispatcher/router.py
@@ -1,7 +1,14 @@
 from config import settings
+from dispatcher import dispatcher
 from fastapi import APIRouter
 
 dispatcherRouter = APIRouter(prefix="/dispatcher")
+dispatcher = dispatcher.Dispatcher(
+    workload_provider=dispatcher.APIWorkloadProvider(settings.base_url),
+    ping_provider=dispatcher.APIPingProvider(settings.base_url),
+    demorunners=dispatcher.parse_demorunners(settings.demorunners_path),
+    policy=dispatcher.make_policy(settings.policy),
+)
 
 
 @dispatcherRouter.get("/stats", status_code=200)
@@ -9,4 +16,4 @@ def get_demorunners_stats():
     """
     Get workload of all DRs.
     """
-    return settings.dispatcher.get_demorunners_stats()
+    return dispatcher.get_demorunners_stats()
