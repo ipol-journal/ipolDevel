b58b7327730bc77027c84920a27143c158bd6386
Héctor Macías <hectormaciasc@gmail.com>
2026-02-12 16:17:16 +0000

    Enhance parse_iso filter to support multiple ISO datetime formats

--- cp2/ControlPanel/ControlPanel/templatetags/filters.py
+++ cp2/ControlPanel/ControlPanel/templatetags/filters.py
@@ -7,7 +7,33 @@ register = Library()
 
 @register.filter(expects_localtime=True)
 def parse_iso(value):
-    try:
-        return datetime.strptime(value, "%Y-%m-%d %H:%M:%S.%f")
-    except Exception:
-        return datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+    """Parse several common ISO datetime string formats into a datetime.
+
+    Accepts inputs like:
+    - "2026-02-12T09:54:26.879578"
+    - "2026-02-12 09:54:26.879578"
+    - "2026-02-12T09:54:26"
+    - "2026-02-12 09:54:26"
+    - "2026-02-12"
+
+    If `value` is already a datetime, it is returned unchanged. If parsing
+    fails, the original value is returned.
+    """
+    if isinstance(value, datetime):
+        return value
+
+    formats = (
+        "%Y-%m-%dT%H:%M:%S.%f",
+        "%Y-%m-%d %H:%M:%S.%f",
+        "%Y-%m-%dT%H:%M:%S",
+        "%Y-%m-%d %H:%M:%S",
+        "%Y-%m-%d",
+    )
+
+    for fmt in formats:
+        try:
+            return datetime.strptime(value, fmt)
+        except Exception:
+            continue
+
+    return value
