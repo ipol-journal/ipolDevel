9615e6899a632d715a8b1fd4f24c729609f0f2f9
jrajan <60109092+Jyotsnarajan@users.noreply.github.com>
2026-03-27 14:40:33 +0530

    Runtime/error archive link (#234)
    * [core] store failed executions and include download link in email
    
    * [sysadmin] added cleanup of error archive ZIP files after 30 days

--- sysadmin/cleanup.sh
+++ sysadmin/cleanup.sh
@@ -8,3 +8,10 @@ find ${BASE} -maxdepth 2 -ctime +30 -type d -exec rm -rf {} \;
 
 # Remove demo directories without any executions
 find ${BASE} -maxdepth 1 -type d -empty -exec rm -rf {} \;
+
+# Remove error archive ZIP files older than 30 days
+ERROR_ARCHIVES="/home/ipol/ipolDevel/shared_folder/error_archives/"
+
+if [ -d "$ERROR_ARCHIVES" ]; then
+    find ${ERROR_ARCHIVES} -type f -name "*.zip" -mtime +30 -exec rm -f {} \;
+fi
